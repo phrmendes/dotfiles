fd6ab89a1c27a2abfe597272e5ba332f50a712ce
Pedro Mendes <phrmendes@tuta.io>
2022-08-01 18:12:55 -0300

    Added ppa and R package files.

---
+++ r_packages.R
@@ -0,0 +1,32 @@
+install.packages("pacman")
+
+pacman::p_load(
+  tidyverse,
+  data.table,
+  arrow,
+  duckdb,
+  mice,
+  quarto,
+  renv,
+  devtools,
+  usethis,
+  shiny,
+  furrr,
+  future,
+  gh,
+  janitor,
+  markdown,
+  pbapply,
+  styler,
+  lintr,
+  testthat,
+  fs,
+  Rcpp,
+  RcppEigen,
+  writexl,
+  rmarkdown,
+  rstan,
+  sf,
+  parallel,
+  distill
+)

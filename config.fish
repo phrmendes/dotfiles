f2ae9ea1732b8330c19ce817b74436a58150ef21
Pedro Mendes <phrmendes@tuta.io>
2022-08-25 14:56:06 -0300

    Auxiliary files.

---
+++ config.fish
@@ -0,0 +1,17 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+eval (/home/linuxbrew/.linuxbrew/bin/brew shellenv)
+
+function vterm_printf;
+    if [ -n "$TMUX" ]
+        # tell tmux to pass the escape sequences through
+        # (Source: http://permalink.gmane.org/gmane.comp.terminal-emulators.tmux.user/1324)
+        printf "\ePtmux;\e\e]%s\007\e\\" "$argv"
+    else if string match -q -- "screen*" "$TERM"
+        # GNU screen (screen, screen-256color, screen-256color-bce)
+        printf "\eP\e]%s\007\e\\" "$argv"
+    else
+        printf "\e]%s\e\\" "$argv"
+    end
+end

e212da6e3c8aa21d507b9b74d0132eb841d3bb3d
Pedro Mendes <phrmendes@tuta.io>
2022-09-12 15:32:09 -0300

    Revert "Reorganizing code."
    This reverts commit 0c99256b89f5dbb5f6cac92140991eb56b5a9d25.

--- bkp.sh
+++ bkp.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-############################### variables #####################################
+# VARIABLES --------------------
 
 DEB_URLS_FILE="deb_urls.txt"
 PROGRAMS_FILE="programs.txt"
@@ -12,7 +12,7 @@ BLUE="\e[1;34m"
 GREEN="\e[1;32m"
 NO_COLOR="\e[0m"
 
-############################### pre-installation ##############################
+# FUNCTIONS --------------------
 
 requirements () {
     local apps 
@@ -33,102 +33,11 @@ requirements () {
     done
 }
 
-remove_locks () {
-    echo -e "${BLUE}[IN PROGRESS] - Removing locks...${NO_COLOR}"
-    sudo rm /var/lib/dpkg/lock-frontend
-    sudo rm /var/cache/apt/archives/lock
-    echo -e "${GREEN}[DONE] - Locks removed.${NO_COLOR}"
-}
-
-add_i386_architecture () {
-    echo -e "${BLUE}[IN PROGRESS] - Adding x86 architecture...${NO_COLOR}"
-    sudo dpkg --add-architecture i386
-    echo -e "${GREEN}[DONE] - x86 architecture added.${NO_COLOR}"
-}
-
-setup_nix () {
-    echo -e "${BLUE}[IN PROGRESS] - Installing Nix...${NO_COLOR}"
-    sudo mkdir /nix
-    sudo chown -R "$USER" /nix
-    sh <(curl -L https://nixos.org/nix/install) --no-daemon
-    echo -e "${GREEN}[DONE] - Nix installed.${NO_COLOR}"
-}
-
-setup_fish() {
-    chmod +x setup.fish
-    fish setup.fish
-}
-
-remove_installed () {
-    local libreoffice
-    local apps
-
-    libreoffice=$(apt list --installed | grep libreoffice | cut -d "/" -f 1)
-    apps=(geary gnome-calendar gnome-contacts)
-
-    for app in "${apps[@]}"; do
-        echo -e "${BLUE}[IN PROGRESS] - Removing $app...${NO_COLOR}"
-        sudo apt remove "$app" -y
-        echo -e "${GREEN}[DONE] - $app removed.${NO_COLOR}"
-    done
-    
-    for app in $libreoffice; do
-        echo -e "${BLUE}[IN PROGRESS] - Removing $app...${NO_COLOR}"
-        sudo apt remove "$app" -y
-        echo -e "${GREEN}[DONE] - $app removed.${NO_COLOR}"
-    done
-    }
-
-setup_docker () {
-    echo -e "${BLUE}[IN PROGRESS] - Installing Docker...${NO_COLOR}"
-    sudo mkdir -p /etc/apt/keyrings
-    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-    echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
-$(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-    att_repos
-    sudo apt install docker-ce docker-ce-cli containerd.io docker-compose-plugin -y
-    sudo groupadd docker
-    sudo usermod -aG docker "USER"
-    echo -e "${GREEN}[DONE] - Docker installed.${NO_COLOR}"
-}
-
-setup_fonts () {
-    echo -e "${BLUE}[IN PROGRESS] - Setting up fonts...${NO_COLOR}"
-    mkdir -p "$HOME"/.local/share/fonts/NerdFonts
-    unzip NerdFonts.zip
-    cp -r NerdFonts/* "$HOME"/.local/share/fonts/NerdFonts
-    fc-cache -f -v
-    sudo rm -r NerdFonts
-    echo -e "${GREEN}[DONE] - Fonts set up.${NO_COLOR}"
-}
-
-############################ system functions #################################
-
-upgrade () {
-    echo -e "${BLUE}[IN PROGRESS] - Upgrading system...${NO_COLOR}"
-    sudo apt dist-upgrade -y
-    echo -e "${GREEN}[DONE] - System upgraded.${NO_COLOR}"
-}
-
-clean () {
-    echo -e "${BLUE}[IN PROGRESS] - Cleaning system...${NO_COLOR}"
-    sudo apt autoclean
-    sudo apt autoremove -y
-    echo -e "${GREEN}[DONE] - System cleaned.${NO_COLOR}"
-}
-
-att_repos () {
-    echo -e "${BLUE}[IN PROGRESS] - Updating repos...${NO_COLOR}"
-    sudo apt update -y
-    echo -e "${GREEN}[DONE] - Repos updated.${NO_COLOR}"
-}
-
-############################### reading files #################################
-
-reading_programs_file () {
+reading_programs_variables () {
     PROGRAMS_APT=()
     PROGRAMS_FLATPAK=()
-    PROGRAMS_NIX=()
+    PROGRAMS_SNAP=()
+    PROGRAMS_BREW=()
 
     while IFS= read -r line; do
         local str_1
@@ -141,13 +50,15 @@ reading_programs_file () {
             PROGRAMS_APT+=("$str_1")
         elif [[ $str_2 = "flatpak" ]]; then
             PROGRAMS_FLATPAK+=("$str_1")
+        elif [[ $str_2 = "snap" ]]; then
+            PROGRAMS_SNAP+=("$str_1")
         else
-            PROGRAMS_NIX+=("$str_1")
+            PROGRAMS_BREW+=("$str_1")
         fi
     done < $PROGRAMS_FILE 
 }
 
-reading_urls_deb_file () {
+reading_urls_deb () {
     PROGRAMS_DEB=()
 
     while IFS= read -r line; do
@@ -163,17 +74,26 @@ reading_ppas_file () {
     done < $PPAS_URLS_FILE
 }
 
-########################## package installation ###############################
-
-adding_ppas () {
-    echo -e "${BLUE}[IN PROGRESS] - Adding PPAs...${NO_COLOR}"
-    for ppa in "${PPAS[@]}"; do
-        sudo add-apt-repository "$ppa" -y
-    done
-    echo -e "${GREEN}[DONE] - PPAs added.${NO_COLOR}"
+remove_locks () {
+    echo -e "${BLUE}[IN PROGRESS] - Removing locks...${NO_COLOR}"
+    sudo rm /var/lib/dpkg/lock-frontend
+    sudo rm /var/cache/apt/archives/lock
+    echo -e "${GREEN}[DONE] - Locks removed.${NO_COLOR}"
 }
 
-download_deb_files () {
+add_i386_architecture () {
+    echo -e "${BLUE}[IN PROGRESS] - Adding x86 architecture...${NO_COLOR}"
+    sudo dpkg --add-architecture i386
+    echo -e "${GREEN}[DONE] - x86 architecture added.${NO_COLOR}"
+}
+
+att_repos () {
+    echo -e "${BLUE}[IN PROGRESS] - Updating repos...${NO_COLOR}"
+    sudo apt update -y
+    echo -e "${GREEN}[DONE] - Repos updated.${NO_COLOR}"
+}
+
+download_deb () {
     mkdir "$DIR_DOWNLOAD"
     for url in "${PROGRAMS_DEB[@]}"; do
         wget -c "$url" -P "$DIR_DOWNLOAD"
@@ -206,6 +126,26 @@ install_flatpak () {
     done
 }
 
+install_brew () {
+    for program in "${PROGRAMS_BREW[@]}"; do
+        echo -e "${BLUE}[IN PROGRESS] - Installing $program (brew)...${NO_COLOR}"
+        brew install "$program"
+        echo -e "${GREEN}[DONE] - $program installed.${NO_COLOR}"    
+    done
+}
+
+homebrew () {
+    echo -e "${BLUE}[IN PROGRESS] - Installing Homebrew...${NO_COLOR}"
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    test -d ~/.linuxbrew && eval "$(~/.linuxbrew/bin/brew shellenv)"
+    test -d /home/linuxbrew/.linuxbrew && eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+    test -r ~/config/fish/config.fish && echo -e "eval \"\$($(brew --prefix)/bin/brew shellenv)\"" >> ~/config/fish/config.fish
+    echo -e "eval \"\$($(brew --prefix)/bin/brew shellenv)\"" >> ~/config/fish/config.fish
+    test -r ~/.bash_profile && echo "eval \"\$($(brew --prefix)/bin/brew shellenv)\"" >> ~/.bash_profile
+    echo "eval \"\$($(brew --prefix)/bin/brew shellenv)\"" >> ~/.profile
+    echo -e "${GREEN}[DONE] - Homebrew installed.${NO_COLOR}"
+}
+
 install_R_packages () {
     echo -e "${BLUE}[IN PROGRESS] - Installing R dependencies...${NO_COLOR}"
     sudo apt update -qq
@@ -219,49 +159,99 @@ install_R_packages () {
     echo -e "${GREEN}[DONE] - R packages installed.${NO_COLOR}"
 }
 
-install_nix_packages () {
-    for program in "${PROGRAMS_NIX[@]}"; do
-        echo -e "${BLUE}[IN PROGRESS] - Installing $program (nix)...${NO_COLOR}"
-        nix-env -iA nix.pkgs."$program"
-        echo -e "${GREEN}[DONE] - $program installed.${NO_COLOR}"
-    done
+upgrade () {
+    echo -e "${BLUE}[IN PROGRESS] - Upgrading system...${NO_COLOR}"
+    sudo apt dist-upgrade -y
+    echo -e "${GREEN}[DONE] - System upgraded.${NO_COLOR}"
 }
 
-install_tutanota () {
+clean () {
+    echo -e "${BLUE}[IN PROGRESS] - Cleaning system...${NO_COLOR}"
+    sudo apt autoclean
+    sudo apt autoremove -y
+    echo -e "${GREEN}[DONE] - System cleaned.${NO_COLOR}"
+}
+
+adding_ppas () {
+    echo -e "${BLUE}[IN PROGRESS] - Adding PPAs...${NO_COLOR}"
+    for ppa in "${PPAS[@]}"; do
+        sudo add-apt-repository "$ppa" -y
+    done
+    echo -e "${GREEN}[DONE] - PPAs added.${NO_COLOR}"
+}
+
+tutanota_download () {
     echo -e "${BLUE}[IN PROGRESS] - Downloading Tutanota...${NO_COLOR}"
     mkdir "$HOME"/appimages/
     wget -c "$TUTANOTA_LINK" -P "$HOME"/appimages/ 
     echo -e "${GREEN}[DONE] - Tutanota downloaded.${NO_COLOR}"
 }
 
-############################## execution ######################################
+remove_installed () {
+    local libreoffice
+    local apps
 
-att_repos
+    libreoffice=$(apt list --installed | grep libreoffice | cut -d "/" -f 1)
+    apps=(geary gnome-calendar gnome-contacts)
+
+    for app in "${apps[@]}"; do
+        echo -e "${BLUE}[IN PROGRESS] - Removing $app...${NO_COLOR}"
+        sudo apt remove "$app" -y
+        echo -e "${GREEN}[DONE] - $app removed.${NO_COLOR}"
+    done
+    
+    for app in $libreoffice; do
+        echo -e "${BLUE}[IN PROGRESS] - Removing $app...${NO_COLOR}"
+        sudo apt remove "$app" -y
+        echo -e "${GREEN}[DONE] - $app removed.${NO_COLOR}"
+    done
+    }
+
+install_docker () {
+    echo -e "${BLUE}[IN PROGRESS] - Installing Docker...${NO_COLOR}"
+    sudo mkdir -p /etc/apt/keyrings
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+    echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+$(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+    att_repos
+    sudo apt install docker-ce docker-ce-cli containerd.io docker-compose-plugin -y
+    sudo groupadd docker
+    sudo usermod -aG docker "USER"
+    echo -e "${GREEN}[DONE] - Docker installed.${NO_COLOR}"
+}
+
+setup_fonts () {
+    echo -e "${BLUE}[IN PROGRESS] - Setting up fonts...${NO_COLOR}"
+    mkdir -p "$HOME"/.local/share/fonts/NerdFonts
+    unzip NerdFonts.zip
+    cp -r NerdFonts/* "$HOME"/.local/share/fonts/NerdFonts
+    fc-cache -f -v
+    sudo rm -r NerdFonts
+    echo -e "${GREEN}[DONE] - Fonts set up.${NO_COLOR}"
+}
+
+# EXECUTION --------------------
+
 requirements
+reading_programs_variables
+reading_urls_deb
+reading_ppas_file
 remove_locks
 add_i386_architecture
-setup_nix
-setup_docker
-setup_fonts
-remove_installed
-clean
 att_repos
-upgrade
-reading_programs_file
-reading_urls_deb_file
-reading_ppas_file
-install_apt
 adding_ppas
-download_deb_files
-att_repos
-install_deb
+upgrade
+homebrew
+remove_installed
 install_apt
+install_deb
 install_flatpak
-install_nix
-install_tutanota
+install_brew
+install_docker
+tutanota_download
 install_R_packages
+setup_fonts
 clean
-setup_fish
 
 sudo rm -r "$DIR_DOWNLOAD"
 

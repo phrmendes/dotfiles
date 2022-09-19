3e9cbac76999012bb96171d075e0416caedefdf7
Pedro Mendes <phrmendes@tuta.io>
2022-09-19 16:52:11 -0300

    Adding fish setup

---
+++ fish_setup.fish
@@ -0,0 +1,29 @@
+# BREW -----
+
+test -d /home/linuxbrew/.linuxbrew && eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+test -r ~/config/fish/config.fish && echo -e "eval \"\$($(brew --prefix)/bin/brew shellenv)\"" >> ~/config/fish/config.fish
+
+# OH MY FISH -----
+
+curl https://raw.githubusercontent.com/oh-my-fish/oh-my-fish/master/bin/install | fish
+omf install agnoster
+omf theme agnoster
+
+# FISHER -----
+
+curl -sL https://git.io/fisher | source && fisher install jorgebucaran/fisher
+
+# FISH CONFIG -----
+
+alias ls="exa --icons"
+alias cat="bat"
+
+# NVM -----
+
+fisher install jorgebucaran/nvm.fish
+echo "nvm use 16 &> /dev/null"  >> .config/fish/config.fish
+
+# LVIM PATH -----
+
+fish_add_path ~/.local/bin/
+fish_add_path ~/.local/share/nvm/v16.17.0/bin/node

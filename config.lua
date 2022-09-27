8467866f501261d68af08089c8982f4b73bb9bce
Pedro Mendes <phrmendes@tuta.io>
2022-09-27 11:38:28 -0300

    Updating neovim dotfile.

--- config.lua
+++ config.lua
@@ -248,8 +248,12 @@ lvim.plugins = {
   },
   -- preview markdown in neovim
   {
-    "npxbr/glow.nvim",
-    ft = { "markdown" }
+    "iamcco/markdown-preview.nvim",
+    run = "cd app && npm install",
+    ft = "markdown",
+    config = function()
+      vim.g.mkdp_auto_start = 1
+    end,
   },
   -- indentation guides for every line
   {

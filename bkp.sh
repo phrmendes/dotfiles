afdb73836f00c87c250e4bd1c2f0c56bee27a48f
Pedro Mendes <phrmendes@tuta.io>
2022-10-07 16:28:32 -0300

    Vim-plug installation.

--- bkp.sh
+++ bkp.sh
@@ -262,6 +262,7 @@ setup_fish () {
 }
 
 setup_neovim_vscode () {
+    sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
     mkdir -p "$HOME"/.config/nvim
     cp ./init.vim "$HOME"/.config/nvim
 }

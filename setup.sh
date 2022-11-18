805e73ddb7bc49f923f2981a4e104f19d70c6fa3
Pedro Mendes <phrmendes@tuta.io>
2022-11-18 20:24:50 -0300

    Fixing home-manager installation.

--- setup.sh
+++ setup.sh
@@ -15,6 +15,7 @@ sudo apt full-upgrade -y
 
 while IFS= read line; do
     str_1=$(echo -e "${line%%,*}")
+sudo apt dist-upgrade -y
     str_2=$(echo -e "${line##*,}")
 
     if [[ $str_2 = "apt" ]]; then
@@ -34,22 +35,19 @@ sudo rm /var/lib/dpkg/lock-frontend
 sudo rm /var/cache/apt/archives/lock
 sudo dpkg --add-architecture i386
 
-for app in "${APPS_TO_INSTALL[@]}"; do
+for app in "${APPS_TO_UNINSTALL[@]}"; do
     sudo apt remove "$app" -y
 done
 
 sh <(curl -L https://nixos.org/nix/install) --daemon
 echo 'export XDG_DATA_DIRS="$HOME/.nix-profile/share:$XDG_DATA_DIRS' >> "$HOME/.profile"
 source "$HOME/.profile"
-nix-channel --add https://github.com/nix-community/home-manager/archive/master.tar.gz home-manager
-nix-channel --update
-echo 'export NIX_PATH=$HOME/.nix-defexpr/channels:/nix/var/nix/profiles/per-user/root/channels${NIX_PATH:+:$NIX_PATH}' >> "$HOME/.profile"
-source "$HOME/.profile"
-nix-shell '<home-manager>' -A install
 
-sudo cp "$MAIN_DIR/aux_files/SauceCodePro.zip" "/usr/share/fonts"
-sudo unzip "/usr/share/fonts/SauceCodePro.zip"
+sudo mkdir -p "/usr/share/fonts/truetype/sourcecodepro/"
+sudo cp "$MAIN_DIR/aux_files/SauceCodePro.zip" "/usr/share/fonts/truetype/sourcecodepro/"
+sudo cd "/usr/share/fonts/truetype/sourcecodepro/" && sudo unzip SauceCodePro.zip && sudo rm SauceCodePro.zip
 sudo fc-cache
+cd "$MAIN_DIR"
 
 for program in "${APT_PROGRAMS[@]}"; do
     sudo apt install "$program" -y
@@ -59,62 +57,6 @@ for program in "${FLATPAK_PROGRAMS[@]}"; do
     flatpak install "$program" -y
 done
 
-      # let
-      #   my-python-packages = python-packages: with python-packages; [
-      #     pandas
-      #     matplotlib
-      #     numpy
-      #     scipy
-      #     scikit-learn
-      #     pyarrow
-      #     sympy
-      #   ];
-      #   python-with-my-packages = python310.withPackages my-python-packages;
-      #   my-r-packages = rWrapper.override{
-      #     packages = with rPackages;
-      #       [
-      #         ggplot2
-      #         dplyr
-      #         dbplyr
-      #         dtplyr
-      #         tidyr
-      #         readr
-      #         purrr
-      #         tibble
-      #         stringr
-      #         forcats
-      #         DBI
-      #         glue
-      #         data_table
-      #         quarto
-      #         janitor
-      #         pbapply
-      #         styler
-      #         zoo
-      #         xts
-      #         lintr
-      #         fs
-      #         distill
-      #         tinytex
-      #         languageserver
-      #         writexl
-      #         devtools
-      #         usethis
-      #         assertthat
-      #         testthat
-      #       ];
-      #   };
-      # in [
-
-stow --target="$HOME" --dir="$MAIN_DIR" --stow .dotfiles
-
 sudo cp "$HOME/Projects/bkps/aux_files/phrmendes" "/var/lib/AccountsService/users/"
 gsettings set org.gnome.desktop.background picture-uri "file://$HOME/.imgs/wallpaper.png"
-
 gsettings set org.gnome.desktop.interface enable-animations false
-
-git clone --depth 1 https://github.com/doomemacs/doomemacs ~/.emacs.d
-"$HOME/.emacs.d/bin/doom" install
-"$HOME/.emacs.d/bin/doom" sync
-
-home-manager switch

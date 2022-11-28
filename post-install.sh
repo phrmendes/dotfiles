180a353a95663962da8fcf2e6898efbf0f731cf9
Pedro Mendes <phrmendes@tuta.io>
2022-11-28 12:16:21 -0300

    Update post-install.sh

--- post-install.sh
+++ post-install.sh
@@ -3,8 +3,15 @@
 # home-manager
 nix-shell '<home-manager>' -A install
 rm "$HOME/.config/nixpkgs/home.nix"
+rm "$HOME/.profile"
 stow --target="$HOME" --dir="$HOME/Projects/bkps" --stow .dotfiles
 home-manager switch
+
+# node
+mkdir "$HOME/.npm-global"
+npm config set prefix '~/.npm-global'
+export PATH="$HOME"/.npm-global/bin:$PATH
+source .profile
 
 # lvim
 LV_BRANCH='release-1.2/neovim-0.8' bash <(curl -s https://raw.githubusercontent.com/lunarvim/lunarvim/master/utils/installer/install.sh)

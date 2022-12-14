cb2593031123e6675fc1c8a963ec1066f2d4d3c0
Pedro Mendes <phrmendes@tuta.io>
2022-12-14 16:08:08 -0300

    Changing script.

--- nix-install.sh
+++ nix-install.sh
@@ -6,14 +6,14 @@ curl -L https://nixos.org/nix/install | sh
 # create .nix-channels file
 touch $HOME/.nix-channels
 
-# install nix-darwin
+# adding darwin channel
 if ! grep -q nix-darwin $HOME/.nix-channels; then
   echo "https://github.com/LnL7/nix-darwin/archive/master.tar.gz darwin" >> $HOME/.nix-channels
 fi
 
 export NIX_PATH=darwin=$HOME/.nix-defexpr/channels/darwin:$NIX_PATH
 
-# install home-manager
+# adding home-manager channel
 if ! grep -q home-manager $HOME/.nix-channels; then
   echo "https://github.com/nix-community/home-manager/archive/master.tar.gz home-manager" >> $HOME/.nix-channels
 fi

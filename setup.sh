226f5b5424f76d48aa5072d5119cb0e9c7579c6f
Pedro Mendes <phrmendes@tuta.io>
2022-12-14 15:49:41 -0300

    Fixing nix-channels file.

--- setup.sh
+++ setup.sh
@@ -7,15 +7,17 @@ xcode-select --install
 curl -L https://nixos.org/nix/install | sh
 
 # install nix-darwin
-if ! grep -q nix-darwin ~/.nix-channels; then
-  echo "https://github.com/LnL7/nix-darwin/archive/master.tar.gz darwin" >> ~/.nix-channels
+if ! grep -q nix-darwin $HOME/.nix-channels; then
+  echo "https://github.com/LnL7/nix-darwin/archive/master.tar.gz darwin" >> $HOME/.nix-channels
 fi
 
 export NIX_PATH=darwin=$HOME/.nix-defexpr/channels/darwin:$NIX_PATH
 
 # install home-manager
-if ! grep -q home-manager ~/.nix-channels; then
-  echo "https://github.com/nix-community/home-manager/archive/master.tar.gz home-manager" >> ~/.nix-channels
+touch $HOME/.nix-channels
+
+if ! grep -q home-manager $HOME/.nix-channels; then
+  echo "https://github.com/nix-community/home-manager/archive/master.tar.gz home-manager" >> $HOME/.nix-channels
 fi
 
 export NIX_PATH=home-manager=$HOME/.nix-defexpr/channels/home-manager:$NIX_PATH

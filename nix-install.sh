9033c4d7b13f457bef4e63e0179e97a46fc69b3c
Pedro Mendes <phrmendes@tuta.io>
2022-12-14 18:31:55 -0300

    Changing nix installation.

--- nix-install.sh
+++ nix-install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sh <(curl -L https://nixos.org/nix/install)
+sh <(curl -L https://nixos.org/nix/install) --darwin-use-unencrypted-nix-store-volume
 
 sudo cat <<EOF >> /etc/zshrc
 if [ -e '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh' ]; then

03e71d22dd969127a39f4fdc6a5675c9b8d11bc8
Pedro Mendes <phrmendes@tuta.io>
2022-12-14 17:12:29 -0300

    Changing script.

--- nix-install.sh
+++ nix-install.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-sh <(curl -L https://nixos.org/nix/install) --darwin-use-unencrypted-nix-store-volume
+sh <(curl -L https://nixos.org/nix/install)
 
-sudo cat <<-EOF >> /etc/zshrc
+sudo cat <<EOF >> /etc/zshrc
 if [ -e '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh' ]; then
   . '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh'
 fi

98f33e8e5c5f892daf6f3295848f44172787d3a5
Pedro Mendes <phrmendes@tuta.io>
2022-12-15 01:10:00 -0300

    Changing tiling window manager.

--- .dotfiles/.nixpkgs/darwin-configuration.nix
+++ .dotfiles/.nixpkgs/darwin-configuration.nix
@@ -42,15 +42,14 @@ in {
     enable = true;
     onActivation.cleanup = "uninstall";
     onActivation.upgrade = true;
-    taps = [
-      "homebrew/cask"
-    ];
+    taps = [ "homebrew/cask" ];
     casks = [
       "keepassxc"
       "mpv"
       "slack"
       "caffeine"
       "maccy"
+      "amethyst"
       "iterm2"
     ];
   };
@@ -101,87 +100,5 @@ in {
       };
     };
   };
-  services = {
-    nix-daemon.enable = true;
-    yabai = {
-      enable = true;
-      package = pkgs.yabai;
-      enableScriptingAddition = true;
-      config = {
-        focus_follows_mouse          = "autoraise";
-        mouse_follows_focus          = "off";
-        window_placement             = "second_child";
-        window_opacity               = "off";
-        window_opacity_duration      = "0.0";
-        window_border                = "off";
-        window_border_placement      = "inset";
-        window_border_width          = 2;
-        window_border_radius         = 3;
-        active_window_border_topmost = "off";
-        window_topmost               = "on";
-        window_shadow                = "float";
-        active_window_border_color   = "0xff5c7e81";
-        normal_window_border_color   = "0xff505050";
-        insert_window_border_color   = "0xffd75f5f";
-        active_window_opacity        = "1.0";
-        normal_window_opacity        = "1.0";
-        split_ratio                  = "0.50";
-        auto_balance                 = "on";
-        mouse_modifier               = "fn";
-        mouse_action1                = "move";
-        mouse_action2                = "resize";
-        layout                       = "bsp";
-        top_padding                  = 5;
-        bottom_padding               = 5;
-        left_padding                 = 5;
-        right_padding                = 5;
-        window_gap                   = 5;
-      };
-      extraConfig = ''
-        # rules
-        yabai -m rule --add app='System Preferences' manage=off
-        # set up spaces
-        yabai -m space 1 --label main
-        yabai -m space 2 --label term
-        yabai -m space 3 --label browser
-        yabai -m space 4 --label slack
-        yabai -m space --focus main
-        # window border
-        yabai -m config window_border on
-        yabai -m config window_border_width 5
-        yabai -m config active_window_border_color color 0x00000000
-        yabai -m config normal_window_border_color 0x00000000
-      '';
-    };
-    skhd = {
-      enable = true;
-      skhdConfig = ''
-        # open terminal
-        shift + alt - return : open -a iterm2 --new
-        # space focus
-        ctrl - left : yabai -m space --focus prev
-        ctrl - right : yabai -m space --focus next
-        lalt - 1 : yabai -m space --focus 1
-        lalt - 2 : yabai -m space --focus 2
-        lalt - 3 : yabai -m space --focus 3
-        lalt - 4 : yabai -m space --focus 4
-        # navigate windows on space
-        lalt - j : yabai -m window --focus next
-        lalt - k : yabai -m window --focus prev
-        # move windows on space
-        shift + lalt - j : yabai -m window --swap next
-        shift + lalt - k : yabai -m window --swap prev
-        # window resizing, resizes the current window larger or smaller on the x-axis
-        lalt - h : yabai -m window --resize right:-20:0 2> /dev/null || yabai -m window --resize left:-20:0
-        lalt - l : yabai -m window --resize right:20:0 2> /dev/null || yabai -m window --resize left:20:0
-        # move window to different space
-        shift + lalt - 1 : yabai -m window --space 1
-        shift + lalt - 2 : yabai -m window --space 2
-        shift + lalt - 3 : yabai -m window --space 3
-        shift + lalt - 4 : yabai -m window --space 4
-        # toggle float and center
-        lalt - t : yabai -m window --toggle float; yabai -m window --grid 4:4:1:1:2:2
-      '';
-    };
-  };
+  services.nix-daemon.enable = true;
 }

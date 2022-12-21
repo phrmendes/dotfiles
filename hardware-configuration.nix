24d5bd79c6990c6ed2f2c73be12ea8d555f054ec
Pedro Mendes <phrmendes@tuta.io>
2022-12-20 23:56:24 -0300

    Changing indentation.

--- hardware-configuration.nix
+++ hardware-configuration.nix
@@ -8,21 +8,16 @@
     kernelModules = [ ];
     extraModulePackages = [ ];
   };
-  fileSystems."/" =
-    { device = "/dev/disk/by-uuid/55576cfc-09c8-473e-8e57-92f182579231";
-      fsType = "ext4";
-    };
+  fileSystems."/" = {
+    device = "/dev/disk/by-uuid/55576cfc-09c8-473e-8e57-92f182579231";
+    fsType = "ext4";
+  };
   boot.initrd.luks.devices."cryptroot".device = "/dev/disk/by-uuid/db084812-d6c8-4a45-9f32-31fec142234b";
-  fileSystems."/boot" =
-    { device = "/dev/disk/by-uuid/F6DE-EAFD";
-      fsType = "vfat";
-    };
-  swapDevices = [
-    {
-      device = "/swapfile";
-      size = 10000;
-    }
-  ];
+  fileSystems."/boot" = {
+    device = "/dev/disk/by-uuid/F6DE-EAFD";
+    fsType = "vfat";
+  };
+  swapDevices = [ {device = "/swapfile"; size = 10000;} ];
   networking.useDHCP = lib.mkDefault true;
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   powerManagement.cpuFreqGovernor = lib.mkDefault "powersave";

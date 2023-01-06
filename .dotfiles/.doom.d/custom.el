92efd3343e3dfea2646854c0b5aeff90bfc212c4
Pedro Mendes <phrmendes@tuta.io>
2023-01-06 18:36:56 -0300

    Adding texlive full.

--- .dotfiles/.doom.d/custom.el
+++ .dotfiles/.doom.d/custom.el
@@ -4,7 +4,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(inferior-ess-r-program
-   "/nix/store/w06n1dx3vp59p9bsh9nasy0i3kgd24x8-R-4.2.2-wrapper/bin/R"))
+   "/nix/store/pkgk29v4g7gqhbplvlzhhfsl7qzibhm2-R-4.2.2-wrapper/bin/R"))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.

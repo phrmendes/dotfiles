994b75b98cba11676aaa9a99ca9cc2480e688bb4
Pedro Mendes <phrmendes@tuta.io>
2023-01-06 22:44:42 -0300

    Deleting custom.el

--- .dotfiles/.doom.d/custom.el
+++ .dotfiles/.doom.d/custom.el
@@ -4,7 +4,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(inferior-ess-r-program
-   "/nix/store/pkgk29v4g7gqhbplvlzhhfsl7qzibhm2-R-4.2.2-wrapper/bin/R"))
+   "/nix/store/afqjcwv0jsm4sj360pq93brzj4q9k0nm-R-4.2.2-wrapper/bin/R"))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.

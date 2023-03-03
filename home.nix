6630f056e0ae67a97b09660f46876cb0399d3582
Pedro Mendes <phrmendes00@pm.me>
2023-03-03 01:35:02 -0300

    Changing from neovim to helix.

--- home.nix
+++ home.nix
@@ -84,15 +84,14 @@ in {
         libsForQt5.ktorrent
         libsForQt5.filelight
         libsForQt5.kpmcore
-        libsForQt5.kate
         libsForQt5.syntax-highlighting
         libsForQt5.ark
       ];
       stateVersion = "22.11";
       sessionVariables = {
-        VISUAL = "nvim";
+        VISUAL = "hx";
         TERMINAL = "alacritty";
-        SUDO_EDITOR = "nvim";
+        SUDO_EDITOR = "hx";
       };
     };
     programs = {
@@ -114,7 +113,6 @@ in {
         shellAliases = {
           mkdir = "mkdir -p";
           cat = "${pkgs.bat}/bin/bat";
-          nv = "${pkgs.neovim}/bin/nvim";
           lg = "${pkgs.lazygit}/bin/lazygit";
           ls = "${pkgs.exa}/bin/exa --icons";
           ll = "${pkgs.exa}/bin/exa --icons -l";
@@ -141,80 +139,9 @@ in {
         enable = true;
         extraPackages = (epkgs: [ epkgs.vterm ] );
       };
-      neovim = {
+      helix = {
         enable = true;
-        defaultEditor = true;
-        plugins = with pkgs.vimPlugins; [
-          nvim-web-devicons
-          nvim-tree-lua
-          plenary-nvim
-          vim-nix
-          vim-easymotion
-          vim-commentary
-          vim-gitgutter
-          auto-pairs
-          {
-            plugin = nord-nvim;
-            config = "lua vim.cmd('colorscheme nord')";
-          }
-          {
-            plugin = indent-blankline-nvim;
-            config = "lua require('indent_blankline').setup()";
-          }
-          {
-            plugin = nvim-treesitter;
-            config = ''
-              lua << EOF
-              require('nvim-treesitter.configs').setup {
-                  highlight = {
-                      enable = true,
-                      additional_vim_regex_highlighting = false
-                  }
-              }
-              EOF
-            '';
-          }
-          {
-            plugin = lualine-nvim;
-            config = ''
-              lua << EOF
-              require('lualine').setup {
-                  options = {
-                      icons_enabled = true,
-                      theme = 'nord'
-                  }
-              }
-              EOF
-            '';
-          }
-        ];
-        extraLuaConfig = ''
-          vim.o.background = 'dark'
-          vim.o.clipboard = 'unnamedplus'
-          vim.o.completeopt = 'noinsert,menuone,noselect'
-          vim.o.cursorline = true
-          vim.o.hidden = true
-          vim.o.inccommand = 'split'
-          vim.o.number = true
-          vim.o.relativenumber = true
-          vim.o.splitbelow = true
-          vim.o.splitright = true
-          vim.o.title = true
-          vim.o.wildmenu = true
-          vim.o.expandtab = true
-          vim.o.ttimeoutlen = 0
-          vim.o.shiftwidth = 2
-          vim.o.tabstop = 2
-          vim.o.undofile = true
-          vim.o.smartindent = true
-          vim.o.tabstop = 4
-          vim.o.shiftwidth = 4
-          vim.o.shiftround = true
-          vim.o.expandtab = true
-          vim.o.scrolloff = 3
-        '';
-        vimAlias = true;
-        vimdiffAlias = true;
+        languages = [ "python" "json" "yaml" "bash" "latex" "make" "toml" "tfvars" "dockerfile" "go" ];
       };
       alacritty = {
         enable = true;

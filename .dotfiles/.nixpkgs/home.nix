780557a7118a97376e2f54e57ff8c340d6e3263b
Pedro Mendes <phrmendes00@pm.me>
2023-03-03 01:55:31 -0300

    Replace neovim for helix.

--- .dotfiles/.nixpkgs/home.nix
+++ .dotfiles/.nixpkgs/home.nix
@@ -92,73 +92,9 @@ in
         enable = true;
         enableZshIntegration = true;
       };
-      neovim = {
+      helix = {
         enable = true;
-        plugins = with pkgs.vimPlugins; [
-          nvim-web-devicons
-          nvim-tree-lua
-          plenary-nvim
-          vim-nix
-          vim-easymotion
-          vim-commentary
-          vim-gitgutter
-          auto-pairs
-          {
-            plugin = indent-blankline-nvim;
-            config = "lua require('indent_blankline').setup()";
-          }
-          {
-            plugin = nvim-treesitter;
-            config = ''
-              lua << EOF
-              require('nvim-treesitter.configs').setup {
-                  highlight = {
-                      enable = true,
-                      additional_vim_regex_highlighting = false
-                  }
-              }
-              EOF
-            '';
-          }
-          {
-            plugin = lualine-nvim;
-            config = ''
-              lua << EOF
-              require('lualine').setup {
-                  options = {
-                      icons_enabled = true
-                  }
-              }
-              EOF
-            '';
-          }
-        ];
-        extraConfig = ''
-          set clipboard+=unnamedplus
-          set completeopt+=noinsert,menuone,noselect
-          set cursorline
-          set hidden
-          set inccommand=split
-          set number
-          set relativenumber
-          set splitbelow
-          set splitright
-          set title
-          set wildmenu
-          set expandtab
-          set ttimeoutlen=0
-          set shiftwidth=2
-          set tabstop=2
-          set undofile
-          set smartindent
-          set tabstop=4
-          set shiftwidth=4
-          set shiftround
-          set expandtab
-          set scrolloff=3
-        '';
-        vimAlias = true;
-        vimdiffAlias = true;
+        theme = "base16_transparent";
       };
     };
   };

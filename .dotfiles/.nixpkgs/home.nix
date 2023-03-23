ac10137fec84ee98c2bc7d57457dd55a19777e58
Pedro Mendes <pedro.mendes-ext@ab-inbev.com>
2023-03-23 12:07:54 -0300

    Back to neovim.

--- .dotfiles/.nixpkgs/home.nix
+++ .dotfiles/.nixpkgs/home.nix
@@ -90,9 +90,75 @@ in
         enable = true;
         enableZshIntegration = true;
       };
-      helix = {
+      neovim = {
         enable = true;
-        settings.theme = "base16_terminal";
+        defaultEditor = true;
+        plugins = with pkgs.vimPlugins; [
+          nvim-web-devicons
+          nvim-tree-lua
+          plenary-nvim
+          vim-nix
+          vim-easymotion
+          vim-commentary
+          vim-gitgutter
+          auto-pairs
+          {
+            plugin = indent-blankline-nvim;
+            config = "lua require('indent_blankline').setup()";
+          }
+          {
+            plugin = nvim-treesitter;
+            config = ''
+              lua << EOF
+              require('nvim-treesitter.configs').setup {
+                  highlight = {
+                      enable = true,
+                      additional_vim_regex_highlighting = false
+                  }
+              }
+              EOF
+            '';
+          }
+          {
+            plugin = lualine-nvim;
+            config = ''
+              lua << EOF
+              require('lualine').setup {
+                  options = {
+                      icons_enabled = true,
+                  }
+              }
+              EOF
+            '';
+          }
+        ];
+        extraLuaConfig = ''
+          vim.o.background = 'dark'
+          vim.o.clipboard = 'unnamedplus'
+          vim.o.completeopt = 'noinsert,menuone,noselect'
+          vim.o.cursorline = true
+          vim.o.hidden = true
+          vim.o.inccommand = 'split'
+          vim.o.number = true
+          vim.o.relativenumber = true
+          vim.o.splitbelow = true
+          vim.o.splitright = true
+          vim.o.title = true
+          vim.o.wildmenu = true
+          vim.o.expandtab = true
+          vim.o.ttimeoutlen = 0
+          vim.o.shiftwidth = 2
+          vim.o.tabstop = 2
+          vim.o.undofile = true
+          vim.o.smartindent = true
+          vim.o.tabstop = 4
+          vim.o.shiftwidth = 4
+          vim.o.shiftround = true
+          vim.o.expandtab = true
+          vim.o.scrolloff = 3
+        '';
+        vimAlias = true;
+        vimdiffAlias = true;
       };
     };
   };

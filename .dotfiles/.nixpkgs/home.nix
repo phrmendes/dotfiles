ae7faf323dfe6a3a52348cfe49c520ce955bd5e8
Pedro Mendes <pedro.mendes-ext@ab-inbev.com>
2023-03-27 15:53:14 -0300

    Adding more commands.

--- .dotfiles/.nixpkgs/home.nix
+++ .dotfiles/.nixpkgs/home.nix
@@ -92,7 +92,6 @@ in
       };
       neovim = {
         enable = true;
-        defaultEditor = true;
         plugins = with pkgs.vimPlugins; [
           nvim-web-devicons
           nvim-tree-lua
@@ -125,37 +124,36 @@ in
               lua << EOF
               require('lualine').setup {
                   options = {
-                      icons_enabled = true,
+                      icons_enabled = true
                   }
               }
               EOF
             '';
           }
         ];
-        extraLuaConfig = ''
-          vim.o.background = 'dark'
-          vim.o.clipboard = 'unnamedplus'
-          vim.o.completeopt = 'noinsert,menuone,noselect'
-          vim.o.cursorline = true
-          vim.o.hidden = true
-          vim.o.inccommand = 'split'
-          vim.o.number = true
-          vim.o.relativenumber = true
-          vim.o.splitbelow = true
-          vim.o.splitright = true
-          vim.o.title = true
-          vim.o.wildmenu = true
-          vim.o.expandtab = true
-          vim.o.ttimeoutlen = 0
-          vim.o.shiftwidth = 2
-          vim.o.tabstop = 2
-          vim.o.undofile = true
-          vim.o.smartindent = true
-          vim.o.tabstop = 4
-          vim.o.shiftwidth = 4
-          vim.o.shiftround = true
-          vim.o.expandtab = true
-          vim.o.scrolloff = 3
+        extraConfig = ''
+          set clipboard+=unnamedplus
+          set completeopt+=noinsert,menuone,noselect
+          set cursorline
+          set hidden
+          set inccommand=split
+          set number
+          set relativenumber
+          set splitbelow
+          set splitright
+          set title
+          set wildmenu
+          set expandtab
+          set ttimeoutlen=0
+          set shiftwidth=2
+          set tabstop=2
+          set undofile
+          set smartindent
+          set tabstop=4
+          set shiftwidth=4
+          set shiftround
+          set expandtab
+          set scrolloff=3
         '';
         vimAlias = true;
         vimdiffAlias = true;

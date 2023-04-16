d363f0feb03ed1fbf0fc17f1d651fe7360e69fb5
Pedro Mendes <phrmendes00@pm.me>
2023-04-16 17:33:19 -0300

    Removing jupyterlab and configuring mamba.

--- home.nix
+++ home.nix
@@ -21,7 +21,6 @@ in {
         ansible
         bitwarden
         btop
-        python311Packages.jupyterlab
         cargo
         chromium
         cmdstan
@@ -103,7 +102,17 @@ in {
           nix_update = "sudo nixos-rebuild switch";
           nix_clean = "nix-collect-garbage";
         };
-        shellInit = builtins.readFile ./.dotfiles/.config/fish/shell.fish;
+        shellInit = ''
+          fish_add_path "$HOME/.npm-global/bin"
+
+          dconf load /org/gnome/settings-daemon/plugins/media-keys/ < "$HOME/Projects/bkps/gnome-keybindings/custom-keys.txt"
+          dconf load /org/gnome/desktop/wm/keybindings/ < "$HOME/Projects/bkps/gnome-keybindings/wm-keys.txt"
+          dconf load /org/gnome/shell/extensions/forge/ < "$HOME/Projects/bkps/gnome-keybindings/forge-keys.txt"
+          dconf load /org/gnome/shell/keybindings/ < "$HOME/Projects/bkps/gnome-keybindings/keys.txt"
+
+          eval "$(micromamba shell hook --shell=fish)"
+          micromamba activate
+        '';
       };
       neovim = {
         enable = true;

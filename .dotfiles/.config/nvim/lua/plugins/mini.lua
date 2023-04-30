9c5e575e0709afcc235a086ba871cb6257d8f906
Pedro Mendes <pedro.mendes-ext@ab-inbev.com>
2023-04-30 17:02:30 -0300

    Remaping minimove.

--- .dotfiles/.config/nvim/lua/plugins/mini.lua
+++ .dotfiles/.config/nvim/lua/plugins/mini.lua
@@ -1,3 +1,16 @@
 local mini_move = require("mini.move")
 
-mini_move.setup()
+mini_move.setup({
+	mappings = {
+		-- visual mode
+		left = "<S-h>",
+		right = "<S-l>",
+		down = "<S-j>",
+		up = "<S-k>",
+		-- normal mode
+		line_left = "<S-h>",
+		line_right = "<S-l>",
+		line_down = "<S-j>",
+		line_up = "<S-k>",
+	},
+})

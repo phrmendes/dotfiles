b17370890096c1d2ec8b00a8332f624747db5974
Pedro Mendes <phrmendes@pop-os.localdomain>
2023-05-01 17:02:10 -0300

    Fixing keymaps.

--- .dotfiles/.config/nvim/lua/core/keymaps.lua
+++ .dotfiles/.config/nvim/lua/core/keymaps.lua
@@ -11,13 +11,13 @@ map("i", "jk", "<ESC>", opts) -- exit insert mode pressing 'jk'
 map("t", "jk", "<C-\\><C-n>", opts) -- exit terminal mode pressing 'jk'
 
 -- vimwiki
-map("n", ",<CR>", "<cmd>VimwikiFollowLink<cr>", opts) -- open link
-map("n", ",<Backspace>", "<cmd>VimwikiGoBackLink<cr>", opts) -- go back
-map("n", ",<Tab>", "<cmd>VimwikiNextLink<cr>", opts) -- go to next link
-map("n", ",c", "<cmd>VimwikiToggleListItem<cr>", opts) -- toggle list item
-map("n", ",n", "<cmd>VimwikiNextTask<cr>", opts) -- go to next task
-map("n", ",Left", "<cmd>VimwikiTableMoveColumnLeft<cr>", opts) -- move table column left
-map("n", ",Right", "<cmd>VimwikiTableMoveColumnRight<cr>", opts) -- move table column right
+map("n","<localleader><CR>", "<cmd>VimwikiFollowLink<cr>", opts) -- open link
+map("n","<localleader><Backspace>", "<cmd>VimwikiGoBackLink<cr>", opts) -- go back
+map("n","<localleader><Tab>", "<cmd>VimwikiNextLink<cr>", opts) -- go to next link
+map("n","<localleader>c", "<cmd>VimwikiToggleListItem<cr>", opts) -- toggle list item
+map("n","<localleader>n", "<cmd>VimwikiNextTask<cr>", opts) -- go to next task
+map("n","<localleader>Left", "<cmd>VimwikiTableMoveColumnLeft<cr>", opts) -- move table column left
+map("n","<localleader>Right", "<cmd>VimwikiTableMoveColumnRight<cr>", opts) -- move table column right
 
 -- python debugger
-map("v", "<cmd>lua require('dap-python').debug_selection()<cr>", opts)
+map("v","<localleader>dp","<cmd>lua require('dap-python').debug_selection()<cr>", opts)

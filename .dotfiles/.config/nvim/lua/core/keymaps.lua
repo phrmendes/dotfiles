29503575f77c93f8b7793315ffe41e2ebf1303d4
Pedro Mendes <pedro.mendes-ext@ab-inbev.com>
2023-05-01 16:25:34 -0300

    Fixing keymaps.

--- .dotfiles/.config/nvim/lua/core/keymaps.lua
+++ .dotfiles/.config/nvim/lua/core/keymaps.lua
@@ -7,14 +7,14 @@ g.mapleader = " "
 g.maplocalleader = ","
 
 -- general keymaps
-keymap.set("i", "jk", "<ESC>", opts) -- exit insert mode pressing 'jk'
-keymap.set("t", "jk", "<C-\\><C-n>", opts) -- exit terminal mode pressing 'jk'
+map("i", "jk", "<ESC>", opts) -- exit insert mode pressing 'jk'
+map("t", "jk", "<C-\\><C-n>", opts) -- exit terminal mode pressing 'jk'
 
 -- vimwiki
-keymap.set("n", ",<CR>", "<cmd>VimwikiFollowLink<cr>", opts) -- open link
-keymap.set("n", ",<Backspace>", "<cmd>VimwikiGoBackLink<cr>", opts) -- go back
-keymap.set("n", ",<Tab>", "<cmd>VimwikiNextLink<cr>", opts) -- go to next link
-keymap.set("n", ",c", "<cmd>VimwikiToggleListItem<cr>", opts) -- toggle list item
-keymap.set("n", ",n", "<cmd>VimwikiNextTask<cr>", opts) -- go to next task
-keymap.set("n", ",Left", "<cmd>VimwikiTableMoveColumnLeft<cr>", opts) -- move table column left
-keymap.set("n", ",Right", "<cmd>VimwikiTableMoveColumnRight<cr>", opts) -- move table column right
+map("n", ",<CR>", "<cmd>VimwikiFollowLink<cr>", opts) -- open link
+map("n", ",<Backspace>", "<cmd>VimwikiGoBackLink<cr>", opts) -- go back
+map("n", ",<Tab>", "<cmd>VimwikiNextLink<cr>", opts) -- go to next link
+map("n", ",c", "<cmd>VimwikiToggleListItem<cr>", opts) -- toggle list item
+map("n", ",n", "<cmd>VimwikiNextTask<cr>", opts) -- go to next task
+map("n", ",Left", "<cmd>VimwikiTableMoveColumnLeft<cr>", opts) -- move table column left
+map("n", ",Right", "<cmd>VimwikiTableMoveColumnRight<cr>", opts) -- move table column right

b44042bf828cb1041b46d4909e66c22f2644bfec
Pedro Mendes <phrmendes@pop-os.localdomain>
2023-06-11 00:24:49 -0300

    Updating installation script.

--- setup.sh
+++ setup.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
-APPIMAGES=("pcloud:https://p-def8.pcloud.com/cBZYvCrtdZgNj4RoZZZhq1do7Z2ZZSzRZkZW10DVZAZHkZppZ94ZJ7ZfzZR4ZNVZX5ZGRZ8VZqzZeFZlHZecm6VZiH3dGJDvi3zvgvVdjGzzPJ3ppHLk/pcloud")
 BOLD_GREEN="\e[1;32m"
 CWD=$(pwd)
 END_COLOR="\e[0m"
 FINGERPRINT_PACKAGES=(open-fprintd fprintd-clients python3-validity)
 FLATPAK_PACKAGES=(com.mattjakeman.extensionmanager)
-FPRINT_PPA=ppa:uunicorn/open-fprintd
+FPRINT_PPA="ppa:uunicorn/open-fprintd"
+LOCAL_BIN="$HOME/.local/bin"
 NIX_BIN="/nix/var/nix/profiles/default/bin/"
 PYENV_BIN="$PYENV_PATH/bin/pyenv"
 PYENV_PATH="$HOME/.pyenv"
@@ -14,21 +14,21 @@ PYTHON_BIN="$PYENV_PATH/shims/python"
 PYTHON_PACKAGES=(poetry ptipython)
 USER=$(whoami)
 
-DEB_PACKAGES=(
-	"proton:proton.me/download/bridge/protonmail-bridge_3.0.21-1_amd64.deb"
-	"wezterm:github.com/wez/wezterm/releases/download/20230408-112425-69ae8472/wezterm-20230408-112425-69ae8472.Ubuntu22.04.deb"
+APPIMAGES=(
+	"pcloud:https://p-def8.pcloud.com/cBZYvCrtdZgNj4RoZZZhq1do7Z2ZZSzRZkZW10DVZAZHkZppZ94ZJ7ZfzZR4ZNVZX5ZGRZ8VZqzZeFZlHZecm6VZiH3dGJDvi3zvgvVdjGzzPJ3ppHLk/pcloud"
+	"wezterm:https://github.com/wez/wezterm/releases/download/20230408-112425-69ae8472/WezTerm-20230408-112425-69ae8472-Ubuntu20.04.AppImage"
 )
 
 TO_REMOVE=(
-	gnome-contacts gnome-calendar totem gnome-terminal
-	gnome-orca evince totem xterm fprintd simple-scan
+	gnome-contacts gnome-calendar totem gnome-terminal geary
+	gnome-orca evince totem xterm fprintd simple-scan gparted
 )
 
 REQUIRED_PROGRAMS=(
-	wget git zip unzip gzip curl file gnupg build-essential fonts-dejavu
-	gdebi-core ca-certificates libssl-dev zlib1g-dev libbz2-dev libreadline-dev
-	libsqlite3-dev libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev
-	libffi-dev liblzma-dev file-roller uidmap
+	wget zip unzip gzip curl file gnupg build-essential fonts-dejavu
+	ca-certificates libssl-dev zlib1g-dev libbz2-dev libreadline-dev
+	libsqlite3-dev libncursesw5-dev xz-utils tk-dev libxml2-dev
+	libxmlsec1-dev libffi-dev liblzma-dev file-roller uidmap rar unrar
 )
 
 clean() {
@@ -77,20 +77,6 @@ install_flatpaks() {
 	done
 }
 
-install_deb() {
-	echo -e "${BOLD_GREEN}Installing .deb packages...${END_COLOR}"
-
-	for program in "${DEB_PACKAGES[@]}"; do
-		key="${program%%:*}"   # deletes after ":"
-		value="${program##*:}" # deletes before ":"
-
-		wget -O "/tmp/${key}.deb" "https://${value}"
-		sudo gdebi "/tmp/${key}.deb" -n
-	done
-
-	sudo apt --fix-broken install -y
-}
-
 download_appimages() {
 	echo -e "${BOLD_GREEN}Downloading AppImages...${END_COLOR}"
 
@@ -98,40 +84,39 @@ download_appimages() {
 		key="${program%%:*}"   # deletes after ":"
 		value="${program##*:}" # deletes before ":"
 
-		wget -O "/tmp/${key}.AppImage" "https://${value}"
-		chmod +x "/tmp/${key}.AppImage"
-		mkdir -p "$HOME/AppImage"
-		mv "/tmp/${key}.AppImage" "$HOME/AppImage"
+		mkdir -p "$LOCAL_BIN"
+		wget -O "$LOCAL_BIN/${key}" "https://${value}"
+		chmod +x "$LOCAL_BIN/${key}"
 	done
 }
 
 home_manager_setup() {
 	"$NIX_BIN/nix-channel" --add https://github.com/nix-community/home-manager/archive/master.tar.gz home-manager
 	"$NIX_BIN/nix-channel" --update
-	export NIX_PATH="$HOME/.nix-defexpr/channels:/nix/var/nix/profiles/per-user/root/channels${NIX_PATH:+:$NIX_PATH}"
 	"$NIX_BIN/nix-shell" '<home-manager>' -A install
+
+	export NIX_PATH="$HOME/.nix-defexpr/channels:/nix/var/nix/profiles/per-user/root/channels${NIX_PATH:+:$NIX_PATH}"
+	export NIXPKGS_ALLOW_UNFREE=1
+
 	rm "$HOME"/.config/home-manager/*
 	ln -s "$CWD/nix/home.nix" "$HOME/.config/home-manager/home.nix"
 	"$HOME/.nix-profile/bin/home-manager" switch
 }
 
 setup_python() {
-	curl https://pyenv.run | bash
+	echo -e "${BOLD_GREEN}Setting up python...${END_COLOR}"
+	sudo zypper install "${PYTHON_REQUIREMENTS[@]}"
 	"$PYENV_BIN" install 3.11.3
 	"$PYENV_BIN" global 3.11.3
-}
 
-install_py_packages() {
 	echo -e "${BOLD_GREEN}Installing python packages...${END_COLOR}"
 	"$PYTHON_BIN" -m pip install --upgrade pip
 	"$PYTHON_BIN" -m pip install "${PYTHON_PACKAGES[@]}"
-}
 
-setup_poetry() {
+	echo -e "${BOLD_GREEN}Setting up poetry...${END_COLOR}"
 	"$PYTHON_BIN" -m poetry config virtualenvs.in-project true
-}
 
-setup_python_debugger() {
+	echo -e "${BOLD_GREEN}Setting up python debugger...${END_COLOR}"
 	mkdir "$HOME/.virtualenvs"
 	cd "$HOME/.virtualenvs" || exit
 	"$PYTHON_BIN" -m venv debugpy
@@ -155,17 +140,9 @@ install_required_programs
 remove_programs
 install_nix
 install_flatpaks
-install_deb
+download_appimages
 home_manager_setup
 setup_python
-install_py_packages
-setup_poetry
-setup_python_debugger
-download_appimages
 
-echo -e "Configure fingerprint? [y/n]"
-read -r fingerprint
-
-if [[ "$fingerprint" == "y" ]]; then
-	fingerprint_setup
-fi
+read -rp "Configure fingerprint? [y/n] " fingerprint
+[[ "$fingerprint" == "y" ]] && fingerprint_setup

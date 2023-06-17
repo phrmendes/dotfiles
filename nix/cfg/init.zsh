74cbd7c1ade884e599549952c313a375de3d9d44
Pedro Mendes <phrmendes@pop-os.localdomain>
2023-06-16 23:42:36 -0300

    Add asdf to path.

--- nix/cfg/init.zsh
+++ nix/cfg/init.zsh
@@ -1,7 +1,4 @@
 #!/bin/bash
 
-export PYENV_ROOT="$HOME/.pyenv"
 export WEZTERM_CONFIG_FILE="$HOME/.wezterm.lua"
-command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"
-eval "$(pyenv init -)"
-path+=("$HOME/.local/bin")
+path+=("$HOME/.local/bin" "$HOME/.asdf/shims")

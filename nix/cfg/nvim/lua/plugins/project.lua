e8e0a4859632ba566632048df1bc0da5c33f96ae
Pedro Mendes <phrmendes@pop-os.localdomain>
2023-07-16 00:33:17 -0300

    Formatting file.

--- nix/cfg/nvim/lua/plugins/project.lua
+++ nix/cfg/nvim/lua/plugins/project.lua
@@ -2,7 +2,7 @@ local setup, project = pcall(require, "project_nvim")
 if not setup then return end
 
 project.setup({
-    detection_methods = {"pattern"},
-    patterns = {".git", ">Projects"},
+    detection_methods = { "pattern" },
+    patterns = { ".git", ">Projects" },
     show_hidden = true
 })

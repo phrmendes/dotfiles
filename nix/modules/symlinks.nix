196b1d3b305941a289bd656bcdbafea0301a5f44
Pedro Mendes <phrmendes@pop-os.localdomain>
2023-07-17 15:41:19 -0300

    Removing default python packages.

--- nix/modules/symlinks.nix
+++ nix/modules/symlinks.nix
@@ -3,7 +3,6 @@
     ".config/efm-langserver/config.yaml".source = ../cfg/efm.yaml;
     ".config/nix/nix.conf".source = ../cfg/nix.conf;
     ".config/nixpkgs/config.nix".source = ../cfg/config.nix;
-    ".default-python-packages".source = ../cfg/default-python-packages.txt;
     ".local/bin/preview.sh".source = ../cfg/joshuto/preview.sh;
     ".profile".source = ../cfg/.profile;
     ".wezterm.lua".source = ../cfg/wezterm.lua;

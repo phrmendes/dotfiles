04c7471c660b0daf32f7290e00ffbdb121fc9d44
Pedro Mendes <pedro.mendes-ext@ab-inbev.com>
2023-09-01 11:50:31 -0300

    Fixing cmp mapping.

--- nix/cfg/nvim/lua/plugins/cmp.lua
+++ nix/cfg/nvim/lua/plugins/cmp.lua
@@ -14,8 +14,8 @@ cmp.setup({
         end,
     },
     mapping = cmp.mapping.preset.insert({
-        ["<C-n>"] = cmp.mapping.select_prev_item(), -- previous suggestion
-        ["<C-p>"] = cmp.mapping.select_next_item(), -- next suggestion
+        ["<C-p>"] = cmp.mapping.select_prev_item(), -- previous suggestion
+        ["<C-n>"] = cmp.mapping.select_next_item(), -- next suggestion
         ["<C-u>"] = cmp.mapping.scroll_docs(-4), -- scroll documentation up
         ["<C-d>"] = cmp.mapping.scroll_docs(4), -- scroll documentation down
         ["<C-Space>"] = cmp.mapping.complete(), -- show completion suggestions

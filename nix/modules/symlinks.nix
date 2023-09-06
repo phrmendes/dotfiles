b4be9c9f786857c069943793ef175a387986a018
Pedro Mendes <pedro.mendes-ext@ab-inbev.com>
2023-09-06 13:21:00 -0300

    Changing symlinks.

--- nix/modules/symlinks.nix
+++ nix/modules/symlinks.nix
@@ -1,9 +1,10 @@
 {
   home.file = {
-    ".config/nix/nix.conf".source = ../cfg/nix.conf;
     ".local/bin/preview.sh".source = ../cfg/joshuto/preview.sh;
     ".profile".source = ../cfg/.profile;
     ".wezterm.lua".source = ../cfg/wezterm.lua;
+    ".config/efm-langserver/config.yaml".source = ../cfg/efm.yaml;
+    ".config/nix/nix.conf".source = ../cfg/nix.conf;
     ".config/nvim" = {
       source = ../cfg/nvim;
       recursive = true;

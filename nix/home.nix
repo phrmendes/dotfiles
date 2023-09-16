331739d70b1433ec3f6cca2f8c3d649dce67fa04
Pedro Mendes <pedrohrmendes@proton.me>
2023-09-16 20:32:06 -0300

    Removing vscodium installation.

--- nix/home.nix
+++ nix/home.nix
@@ -18,7 +18,6 @@ in {
     ./modules/starship.nix
     ./modules/symlinks.nix
     ./modules/tmux.nix
-    ./modules/vscodium.nix
     ./modules/zathura.nix
     ./modules/zoxide.nix
     ./modules/zsh.nix

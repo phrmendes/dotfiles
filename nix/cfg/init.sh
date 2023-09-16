4568ebab7968664dc62e61257548cd15a6c7aa09
Pedro Mendes <pedrohrmendes@proton.me>
2023-09-16 02:09:32 -0300

    Changing env vars.

--- nix/cfg/init.sh
+++ nix/cfg/init.sh
@@ -2,14 +2,10 @@
 
 eval "$(micromamba shell hook --shell=zsh)"
 
-export FZF_DEFAULT_OPTS=" \
---color=bg+:#313244,bg:#1e1e2e,spinner:#f5e0dc,hl:#f38ba8 \
---color=fg:#cdd6f4,header:#f38ba8,info:#cba6f7,pointer:#f5e0dc \
---color=marker:#f5e0dc,fg+:#cdd6f4,prompt:#cba6f7,hl+:#f38ba8"
-
 export FLAKE_PATH="$HOME/Projects/bkps/nix#phrmendes"
-
+export FZF_DEFAULT_COMMAND="fd --type f"
 export NOTES_DIR="$HOME/pCloudDrive/notes"
+export FZF_DEFAULT_OPTS="--color=bg+:#313244,bg:#1e1e2e,spinner:#f5e0dc,hl:#f38ba8 --color=fg:#cdd6f4,header:#f38ba8,info:#cba6f7,pointer:#f5e0dc --color=marker:#f5e0dc,fg+:#cdd6f4,prompt:#cba6f7,hl+:#f38ba8"
 
 path+=(
 	"$HOME/.local/bin"

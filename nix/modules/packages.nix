fce8d446c19b7e443c7c651ee9c9c0e9881c5d30
Pedro Mendes <pedrohrmendes@proton.me>
2023-09-24 22:32:34 -0300

    Refactoring packages.

--- nix/modules/packages.nix
+++ nix/modules/packages.nix
@@ -1,42 +1,45 @@
 {pkgs, ...}: {
-  home.packages = with pkgs; [
-    # CLI
-    bitwarden-cli
-    coursier
-    duckdb
-    eza
-    fd
-    gh
-    gnome-extensions-cli
-    hugo
-    hunspell
-    hunspellDicts.en_US
-    hunspellDicts.pt_BR
-    jdk20
-    lazydocker
-    micromamba
-    ncdu
-    pandoc
-    parallel
-    quarto
-    ripgrep
-    scala_3
-    sqlite
-    tealdeer
-    tectonic
-    terraform
-    xclip
-    # GUI
-    bitwarden
-    caffeine-ng
-    chromium
-    deluge
-    droidcam
-    libreoffice
-    logseq
-    syncthing
-    syncthingtray
-    vlc
-    zotero
-  ];
+  home.packages =
+    (with pkgs; [
+      # CLI
+      bitwarden-cli
+      coursier
+      duckdb
+      eza
+      fd
+      gh
+      gnome-extensions-cli
+      hugo
+      hunspell
+      jdk20
+      lazydocker
+      micromamba
+      ncdu
+      pandoc
+      parallel
+      quarto
+      ripgrep
+      scala_3
+      sqlite
+      tealdeer
+      tectonic
+      terraform
+      xclip
+      # GUI
+      bitwarden
+      caffeine-ng
+      chromium
+      deluge
+      droidcam
+      libreoffice
+      logseq
+      syncthing
+      syncthingtray
+      vlc
+      zotero
+    ])
+    ++ (with pkgs.hunspellDicts; [
+      en_US
+      pt_BR
+    ]);
 }

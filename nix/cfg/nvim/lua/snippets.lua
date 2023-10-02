ebb467896f2de7567c19180c86280aeb7c39a80f
Pedro Mendes <pedrohrmendes@proton.me>
2023-10-02 12:55:54 -0300

    Changing snippets.

--- nix/cfg/nvim/lua/snippets.lua
+++ nix/cfg/nvim/lua/snippets.lua
@@ -9,22 +9,7 @@ local t = luasnip.text_node
 -- [[ snippets ]] --------------------------------------------------------
 -- creating unique IDs
 local generate_id = function()
-	local bufname = vim.api.nvim_buf_get_name(0)
-	local filename = vim.fn.fnamemodify(bufname, ":t:r")
-	local sanitized_filename = filename:gsub("[^%w]", "")
-	local parent_folder = vim.fn.fnamemodify(bufname, ":h:t")
-	local date = os.date("%d%m%Y%H%M%S")
-	local id
-
-	if parent_folder == "notes" then
-		id = sanitized_filename .. "-" .. date
-	elseif parent_folder == "diary" then
-		id = parent_folder .. "-" .. date
-	else
-		id = parent_folder .. "-" .. sanitized_filename .. "-" .. date
-	end
-
-	return string.upper(id)
+	return os.date("%d%m%Y%H%M%S")
 end
 
 -- standard metadata

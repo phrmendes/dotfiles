e9c3363a60eea53f4e18defe0fb51d809e531dfd
Pedro Mendes <pedrohrmendes@proton.me>
2023-10-09 20:17:53 -0300

    Adding packages.

--- nix/modules/packages.nix
+++ nix/modules/packages.nix
@@ -1,28 +1,42 @@
 {pkgs, ...}: {
-  home.packages = with pkgs; [
-    # CLI
-    asdf-vm
-    bitwarden-cli
-    docker-slim
-    duckdb
-    eza
-    fd
-    gh
-    gnome-extensions-cli
-    hugo
-    micromamba
-    ncdu
-    pandoc
-    parallel
-    quarto
-    ripgrep
-    sqlite
-    tealdeer
-    tectonic
-    terraform
-    xclip
-    # GUI
-    caffeine-ng
-    droidcam
-  ];
+  home.packages =
+    (with pkgs; [
+      # CLI
+      asdf-vm
+      bitwarden-cli
+      docker-slim
+      duckdb
+      eza
+      fd
+      gh
+      gnome-extensions-cli
+      hugo
+      hunspell
+      micromamba
+      ncdu
+      pandoc
+      parallel
+      quarto
+      ripgrep
+      sqlite
+      tealdeer
+      tectonic
+      terraform
+      xclip
+      # GUI
+      caffeine-ng
+      droidcam
+      bitwarden
+      peek
+      fragments
+      komikku
+      celluloid
+      obsidian
+      libreoffice
+      zotero
+    ])
+    ++ (with pkgs.hunspellDicts; [
+      en-us
+      pt-br
+    ]);
 }

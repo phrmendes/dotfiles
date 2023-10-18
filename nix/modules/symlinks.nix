afc1105669d631419803b9485ff5a1296a95f9f7
Pedro Mendes <pedrohrmendes@proton.me>
2023-10-18 14:30:55 -0300

    Removing Go support.

--- nix/modules/symlinks.nix
+++ nix/modules/symlinks.nix
@@ -1,7 +1,6 @@
 {
   home.file = {
     ".default-python-packages".source = ../cfg/default-python-packages;
-    ".default-go-packages".source = ../cfg/default-go-packages;
     ".profile".source = ../cfg/.profile;
     ".wezterm.lua".source = ../cfg/wezterm.lua;
     ".config/nvim" = {

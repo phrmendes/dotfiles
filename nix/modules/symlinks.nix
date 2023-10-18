71e8511de81fb063a0679d559826c323ceb1ae41
Pedro Mendes <pedrohrmendes@proton.me>
2023-10-18 16:55:50 -0300

    Changing nvim-qt settings.

--- nix/modules/symlinks.nix
+++ nix/modules/symlinks.nix
@@ -3,6 +3,7 @@
     ".default-python-packages".source = ../cfg/default-python-packages;
     ".profile".source = ../cfg/.profile;
     ".wezterm.lua".source = ../cfg/wezterm.lua;
+    ".config/nvim-qt/nvim-qt.conf".source = ../cfg/nvim-qt.conf;
     ".config/nvim" = {
       source = ../cfg/nvim;
       recursive = true;

f9e17a1da70c4d00c95687c7a87ef4c4dd4485c1
Pedro Mendes <pedrohrmendes@proton.me>
2023-10-30 18:30:30 -0300

    Removing desktop entry.

--- nix/modules/xdg.nix
+++ nix/modules/xdg.nix
@@ -1,38 +1,6 @@
-let
-  inherit (import ../parameters.nix) username;
-  bin = "/home/${username}/.nix-profile/bin";
-in {
+{
   xdg = {
     enable = true;
     mime.enable = true;
-    desktopEntries = {
-      nvim = {
-        name = "Neovim";
-        genericName = "Text Editor";
-        comment = "Edit text files";
-        exec = "${bin}/nvim-qt --nvim ${bin}/nvim %F";
-        icon = "nvim";
-        mimeType = [
-          "application/x-shellscript"
-          "text/english"
-          "text/plain"
-          "text/x-c"
-          "text/x-c++"
-          "text/x-c++hdr"
-          "text/x-c++src"
-          "text/x-chdr"
-          "text/x-csrc"
-          "text/x-java"
-          "text/x-makefile"
-          "text/x-moc"
-          "text/x-pascal"
-          "text/x-tcl"
-          "text/x-tex"
-        ];
-        terminal = false;
-        type = "Application";
-        categories = ["Utility" "TextEditor"];
-      };
-    };
   };
 }

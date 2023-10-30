20bd1180cc55655e713c0539f65dac0b5738ab57
Pedro Mendes <pedrohrmendes@proton.me>
2023-10-30 16:32:26 -0300

    Remove nvim-qt.

--- nix/modules/symlinks.nix
+++ nix/modules/symlinks.nix
@@ -3,7 +3,6 @@
     ".default-python-packages".source = ../cfg/default-python-packages;
     ".profile".source = ../cfg/.profile;
     ".wezterm.lua".source = ../cfg/wezterm.lua;
-    ".config/nvim-qt/nvim-qt.conf".source = ../cfg/nvim-qt.conf;
     ".config/nvim" = {
       source = ../cfg/nvim;
       recursive = true;

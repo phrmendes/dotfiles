bdf6f60c5cc9c48fb82476249f89c63f4ca9c296
Pedro Mendes <pedrohrmendes@proton.me>
2023-10-30 16:31:53 -0300

    Reorganizing packages.

--- nix/modules/packages.nix
+++ nix/modules/packages.nix
@@ -1,50 +1,54 @@
-{pkgs, ...}: {
-  home.packages =
-    (with pkgs; [
-      # CLI
-      ansible
-      asdf-vm
-      bitwarden-cli
-      eza
-      fd
-      gh
-      gnome-extensions-cli
-      hugo
-      hunspell
-      jre_minimal
-      micromamba
-      ncdu
-      pandoc
-      parallel
-      podman
-      podman-compose
-      poetry
-      quarto
-      ripgrep
-      sqlite
-      tealdeer
-      tectonic
-      terraform
-      vimv-rs
-      xclip
-      # GUI
-      bitwarden
-      caffeine-ng
-      dbeaver
-      droidcam
-      evince
-      libreoffice
-      neovim-qt
-      obsidian
-      peek
-      podman-desktop
-      qbittorrent
-      thunderbird
-      vlc
-      zotero
-    ])
-    ++ (with pkgs.hunspellDicts; [
-      en-us
-      pt-br
-    ]);
+{
+  pkgs,
+  lib,
+  ...
+}: let
+  concat = lib.lists.concatLists;
+  cli = with pkgs; [
+    ansible
+    asdf-vm
+    bitwarden-cli
+    eza
+    fd
+    gh
+    gnome-extensions-cli
+    hugo
+    micromamba
+    ncdu
+    pandoc
+    parallel
+    podman
+    podman-compose
+    poetry
+    quarto
+    ripgrep
+    tealdeer
+    tectonic
+    terraform
+    vimv-rs
+  ];
+  gui = with pkgs; [
+    bitwarden
+    caffeine-ng
+    dbeaver
+    droidcam
+    evince
+    libreoffice
+    obsidian
+    peek
+    podman-desktop
+    qbittorrent
+    thunderbird
+    vlc
+    zotero
+  ];
+  dependencies = with pkgs; [
+    jre_minimal
+    xclip
+    hunspell
+    hunspellDicts.en-us
+    hunspellDicts.pt-br
+  ];
+in {
+  home.packages = concat [gui cli dependencies];
 }

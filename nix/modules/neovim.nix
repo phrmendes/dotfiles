982757fbdd1d43343f7d83c318b09f35aa8b11a4
Pedro Mendes <pedrohrmendes@proton.me>
2023-10-30 16:32:02 -0300

    Reorganizing neovim packages.

--- nix/modules/neovim.nix
+++ nix/modules/neovim.nix
@@ -3,6 +3,7 @@
   lib,
   ...
 }: let
+  concat = lib.lists.concatLists;
   fromGitHub = rev: ref: repo:
     pkgs.vimUtils.buildVimPluginFrom2Nix {
       pname = "${lib.strings.sanitizeDerivationName repo}";
@@ -12,11 +13,132 @@
         url = "https://github.com/${repo}.git";
       };
     };
-  cmp-zotcite = fromGitHub "431c720a73fb84c8b1a51b4f123e2a7cc8a58bfd" "HEAD" "jalvesaq/cmp-zotcite";
-  conform-nvim = fromGitHub "43d2b5c6a254f60cbd2142345d2f903e04f9db07" "HEAD" "stevearc/conform.nvim";
-  telescope-bibtex = fromGitHub "e4dcf64d351db23b14be3563190cf68d5cd49e90" "HEAD" "nvim-telescope/telescope-bibtex.nvim";
-  zotcite = fromGitHub "442519a20a80b9ccc8a2baa0607080a21c4ecee0" "HEAD" "jalvesaq/zotcite";
-  obsidian-nvim = fromGitHub "630c92045f0595123031b598c624dbad3b5f6dbc" "HEAD" "epwalsh/obsidian.nvim";
+  gh = {
+    cmp-zotcite = fromGitHub "431c720a73fb84c8b1a51b4f123e2a7cc8a58bfd" "HEAD" "jalvesaq/cmp-zotcite";
+    conform-nvim = fromGitHub "43d2b5c6a254f60cbd2142345d2f903e04f9db07" "HEAD" "stevearc/conform.nvim";
+    telescope-bibtex = fromGitHub "e4dcf64d351db23b14be3563190cf68d5cd49e90" "HEAD" "nvim-telescope/telescope-bibtex.nvim";
+    zotcite = fromGitHub "442519a20a80b9ccc8a2baa0607080a21c4ecee0" "HEAD" "jalvesaq/zotcite";
+    obsidian-nvim = fromGitHub "630c92045f0595123031b598c624dbad3b5f6dbc" "HEAD" "epwalsh/obsidian.nvim";
+  };
+  completion = with pkgs.vimPlugins; [
+    cmp-nvim-lsp
+    cmp-pandoc-nvim
+    cmp-path
+    cmp_luasnip
+    nvim-cmp
+    gh.cmp-zotcite
+  ];
+  lsp = with pkgs.vimPlugins; [
+    lsp_signature-nvim # lsp signature hint as you type
+    lspkind-nvim # vscode-like pictograms
+    ltex_extra-nvim # ltex-ls aditional options
+    nvim-lint # linting
+    nvim-lspconfig
+    gh.conform-nvim # formatting
+  ];
+  telescope = with pkgs.vimPlugins; [
+    telescope-dap-nvim
+    telescope-fzy-native-nvim
+    telescope-nvim
+    telescope-symbols-nvim
+    telescope-ui-select-nvim
+    telescope-zoxide
+    gh.telescope-bibtex
+  ];
+  treesitter = with pkgs.vimPlugins; [
+    nvim-treesitter-context
+    nvim-treesitter-textobjects
+    nvim-treesitter.withAllGrammars
+  ];
+  dap = with pkgs.vimPlugins; [
+    nvim-dap
+    nvim-dap-ui
+    nvim-dap-virtual-text
+    nvim-dap-python
+  ];
+  git = with pkgs.vimPlugins; [
+    lazygit-nvim
+    gitsigns-nvim
+    vim-fugitive
+  ];
+  writing = with pkgs.vimPlugins; [
+    quarto-nvim
+    markdown-preview-nvim
+    nabla-nvim # math symbols
+    gh.zotcite
+    gh.obsidian-nvim
+  ];
+  ia = with pkgs.vimPlugins; [
+    copilot-vim
+    ChatGPT-nvim
+  ];
+  style = with pkgs.vimPlugins; [
+    catppuccin-nvim
+    nvim-web-devicons
+    which-key-nvim
+  ];
+  snippets = with pkgs.vimPlugins; [
+    friendly-snippets
+    luasnip
+    neogen # auto-generate docs
+  ];
+  languages-support = with pkgs.vimPlugins; [
+    vim-jinja
+    vim-nix
+    vim-helm
+  ];
+  text = with pkgs.vimPlugins; [
+    comment-nvim
+    dial-nvim # increment/decrement text objects
+    todo-comments-nvim # highlight todo comments
+    vim-illuminate # highlight word under cursor
+  ];
+  utils = with pkgs.vimPlugins; [
+    executor-nvim # run async jobs
+    iron-nvim # REPLs
+    mini-nvim # set of small plugins
+    nvim-bqf # better quickfix
+    nvim-tree-lua # file explorer
+    undotree # undo tree
+    vim-tmux-navigator # tmux keybindings
+    vim-visual-multi # multiple cursors
+    zen-mode-nvim # zen mode
+  ];
+  dependencies = with pkgs.vimPlugins; [
+    nui-nvim # ui component lib
+    otter-nvim # code completion for code embedded in documents
+    plenary-nvim # lua utils
+    popup-nvim
+  ];
+  language-servers = with pkgs; [
+    ansible-language-server
+    docker-compose-language-service
+    ltex-ls
+    lua-language-server
+    nil
+    ruff-lsp
+    taplo
+    terraform-ls
+    texlab
+    nodePackages.bash-language-server
+    nodePackages.dockerfile-language-server-nodejs
+    nodePackages.pyright
+    nodePackages.vscode-json-languageserver
+    nodePackages.yaml-language-server
+  ];
+  linters = with pkgs; [
+    ansible-lint
+    shellcheck
+    statix
+  ];
+  formatters = with pkgs; [
+    alejandra
+    ruff
+    shellharden
+    stylua
+    nodePackages.prettier
+    perl538Packages.LatexIndent
+  ];
 in {
   programs.neovim = {
     enable = true;
@@ -25,69 +147,21 @@ in {
     vimdiffAlias = true;
     withNodeJs = true;
     withPython3 = true;
-    plugins = with pkgs.vimPlugins; [
-      ChatGPT-nvim # chatgpt
-      catppuccin-nvim # colorscheme
-      cmp-nvim-lsp # lsp completion
-      cmp-pandoc-nvim # pandoc completion
-      cmp-path # path completion
-      cmp-zotcite # zotero completion
-      cmp_luasnip # snippets completion
-      comment-nvim # comments
-      conform-nvim # formatting
-      copilot-vim # github copilot
-      dial-nvim # increment/decrement
-      executor-nvim # async jobs
-      friendly-snippets # snippets
-      gitsigns-nvim # git indicators
-      iron-nvim # REPLs
-      lazygit-nvim # git ui
-      lsp_signature-nvim # lsp signature
-      lspkind-nvim # vscode-like pictograms
-      ltex_extra-nvim # ltex-ls utils
-      luasnip # snippets
-      markdown-preview-nvim # markdown preview
-      mini-nvim # set of small plugins
-      nabla-nvim # colorscheme
-      neogen # auto-generate docs
-      nui-nvim # neovim ui component lib
-      nvim-bqf # quickfix
-      nvim-cmp # completion
-      nvim-dap # debug adapter protocol
-      nvim-dap-python # python dap support
-      nvim-dap-ui # ui for dap
-      nvim-dap-virtual-text # virtual text for dap
-      nvim-lint # linting
-      nvim-lspconfig # lsp
-      nvim-tree-lua # file explorer
-      nvim-treesitter-context # treesitter context
-      nvim-treesitter-textobjects # treesitter textobjects
-      nvim-treesitter.withAllGrammars # treesitter
-      nvim-web-devicons # icons
-      obsidian-nvim # obsidian integration
-      otter-nvim # quarto dependency
-      plenary-nvim # lua utils
-      popup-nvim # zoxide dependency
-      quarto-nvim # quarto integration
-      telescope-bibtex # bibtex integration
-      telescope-dap-nvim # dap integration
-      telescope-fzy-native-nvim # telescope fzy integration
-      telescope-nvim # fuzzy finder
-      telescope-symbols-nvim # undo integration
-      telescope-ui-select-nvim # telescope ui
-      telescope-zoxide # zoxide integration
-      todo-comments-nvim # todo comments
-      undotree # undo tree
-      vim-fugitive # git integration
-      vim-helm # helm syntax
-      vim-illuminate # highlight word under cursor
-      vim-jinja # jinja syntax
-      vim-nix # nix syntax
-      vim-tmux-navigator # tmux keybindings
-      vim-visual-multi # multiple cursors
-      which-key-nvim # keybindings
-      zen-mode-nvim # zen mode
-      zotcite # zotero integration
+    plugins = concat [
+      completion
+      dap
+      dependencies
+      git
+      ia
+      languages-support
+      lsp
+      snippets
+      style
+      telescope
+      text
+      treesitter
+      utils
+      writing
     ];
     extraPython3Packages = pyPkgs:
       with pyPkgs; [
@@ -96,36 +170,10 @@ in {
         pyqt5
         pyyaml
       ];
-    extraPackages =
-      (with pkgs; [
-        alejandra
-        ansible-language-server
-        ansible-lint
-        docker-compose-language-service
-        ltex-ls
-        lua-language-server
-        metals
-        nil
-        ruff
-        ruff-lsp
-        shellcheck
-        shellharden
-        statix
-        stylua
-        taplo
-        terraform-ls
-        texlab
-      ])
-      ++ (with pkgs.nodePackages; [
-        bash-language-server
-        dockerfile-language-server-nodejs
-        prettier
-        pyright
-        vscode-json-languageserver
-        yaml-language-server
-      ])
-      ++ (with pkgs.perl538Packages; [
-        LatexIndent
-      ]);
+    extraPackages = concat [
+      language-servers
+      linters
+      formatters
+    ];
   };
 }

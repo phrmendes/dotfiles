adc33b978ef538d307957feeb4b701eb0578a5b1
Pedro Mendes <pedrohrmendes@proton.me>
2023-11-27 13:48:09 -0300

    Only load image preview when not in neovide.

--- dotfiles/nvim/lua/plugins/image.lua
+++ dotfiles/nvim/lua/plugins/image.lua
@@ -1,16 +1,18 @@
-local image = require("image")
+if not vim.g.neovide then
+	local image = require("image")
 
-image.setup({
-	backend = "ueberzug",
-	integrations = {
-		markdown = {
-			enabled = true,
-			only_render_image_at_cursor = true,
-			filetypes = { "markdown", "quarto" },
+	image.setup({
+		backend = "ueberzug",
+		integrations = {
+			markdown = {
+				enabled = true,
+				only_render_image_at_cursor = true,
+				filetypes = { "markdown", "quarto" },
+			},
 		},
-	},
-	max_width = 100,
-	max_height = 15,
-	editor_only_render_when_focused = false,
-	tmux_show_only_in_active_window = true,
-})
+		max_width = 100,
+		max_height = 15,
+		editor_only_render_when_focused = false,
+		tmux_show_only_in_active_window = true,
+	})
+end

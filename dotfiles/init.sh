99c3a9ddeb5bd572e44b3ee1ef52a09fb39df8c6
Pedro Mendes <pedrohrmendes@proton.me>
2023-11-27 00:57:33 -0300

    Update zsh settings.

--- dotfiles/init.sh
+++ dotfiles/init.sh
@@ -2,24 +2,12 @@
 
 eval "$(micromamba shell hook --shell=zsh)"
 
-export FLAKE_PATH="$HOME"/Projects/bkps
-export NOTES="$HOME"/pCloud/notes
-
 path+=(
 	"$HOME"/.local/bin
 	"$HOME"/.local/share/coursier/bin
-	"$HOME"/.nix-profile/bin
 )
 
-function nix_update() {
-	sudo nixos-rebuild switch --flake "$FLAKE_PATH"
-}
-
-function nix_clear() {
-	nix store gc --debug
-}
-
-function get_gh_repo_ref() {
+function gh_repo_ref() {
 	gh_repo="$1"
 	branch="$2"
 	git ls-remote "https://github.com/$gh_repo" "$branch" | cut -f1

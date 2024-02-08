175a4be91738a53f3ac41ff2cb4636e19352aa28
Pedro Mendes <pedrohrmendes@proton.me>
2024-02-08 18:51:54 -0300

    Changing symbols in lualine.

--- dotfiles/nvim/lua/plugins/lualine.lua
+++ dotfiles/nvim/lua/plugins/lualine.lua
@@ -2,11 +2,11 @@ local lualine = require("lualine")
 
 local venv = function()
 	if vim.env.CONDA_DEFAULT_ENV then
-		return string.format("󱔎 %s (conda)", vim.env.CONDA_DEFAULT_ENV)
+		return string.format("󱔎 %s", vim.env.CONDA_DEFAULT_ENV)
 	end
 
 	if vim.env.VIRTUAL_ENV then
-		return string.format(" %s (venv)", vim.fn.fnamemodify(vim.env.VIRTUAL_ENV, ":t"))
+		return string.format(" %s", vim.fn.fnamemodify(vim.env.VIRTUAL_ENV, ":t"))
 	end
 
 	return ""

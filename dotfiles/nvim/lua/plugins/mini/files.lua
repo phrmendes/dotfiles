b038b23c5101c1cbc092f5300de9e0af81fee9b3
Pedro Mendes <pedrohrmendes@proton.me>
2024-02-29 13:01:03 -0300

    Update files commands.

--- dotfiles/nvim/lua/plugins/mini/files.lua
+++ dotfiles/nvim/lua/plugins/mini/files.lua
@@ -4,9 +4,9 @@ local files = require("mini.files")
 files.setup({
 	mappings = {
 		close = "q",
-		go_in = "<TAB>",
+		go_in = "l",
 		go_in_plus = "<CR>",
-		go_out = "<S-TAB>",
+		go_out = "h",
 		go_out_plus = "<BS>",
 		reset = "<BS>",
 		reveal_cwd = "@",
@@ -20,15 +20,34 @@ files.setup({
 map({
 	key = "<leader>e",
 	cmd = function()
-		files.open(vim.api.nvim_buf_get_name(0), true)
+		if not files.close() then
+			files.open()
+		end
 	end,
-	desc = "Open file explorer (cwd)",
+	desc = "Open file explorer",
 })
 
-map({
-	key = "<leader>E",
-	cmd = function()
-		files.open(vim.loop.cwd(), true)
+-- toggle dotfiles
+local show_dotfiles = true
+
+local filter_show = function()
+	return true
+end
+
+local filter_hide = function(fs_entry)
+	return not vim.startswith(fs_entry.name, ".")
+end
+
+local toggle_dotfiles = function()
+	show_dotfiles = not show_dotfiles
+	local new_filter = show_dotfiles and filter_show or filter_hide
+	files.refresh({ content = { filter = new_filter } })
+end
+
+vim.api.nvim_create_autocmd("User", {
+	pattern = "MiniFilesBufferCreate",
+	callback = function(args)
+		local buf_id = args.data.buf_id
+		vim.keymap.set("n", ".", toggle_dotfiles, { buffer = buf_id })
 	end,
-	desc = "Open file explorer (root)",
 })

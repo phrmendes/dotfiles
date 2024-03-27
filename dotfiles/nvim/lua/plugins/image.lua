ff9ed412ab0f0a426d512f9b166d80ad573f9583
Pedro Mendes <pedrohrmendes@proton.me>
2024-03-27 02:11:39 -0300

    Change image settings.

--- dotfiles/nvim/lua/plugins/image.lua
+++ dotfiles/nvim/lua/plugins/image.lua
@@ -6,15 +6,12 @@ later(function()
 		integrations = {
 			markdown = {
 				enabled = true,
-				only_render_image_at_cursor = true,
 				download_remote_images = true,
 				filetypes = { "markdown", "quarto" },
 			},
 		},
 		max_width = 100,
 		max_height = 15,
-		editor_only_render_when_focused = false,
-		tmux_show_only_in_active_window = true,
 	})
 
 	require("img-clip").setup({

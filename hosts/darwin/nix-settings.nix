bc69cf1c82a9f86891dbe48b71a0db1f1b1ccfb6
Pedro Mendes <pedrohrmendes@proton.me>
2024-04-24 15:08:48 -0300

    Auto-accept flake config.

--- hosts/darwin/nix-settings.nix
+++ hosts/darwin/nix-settings.nix
@@ -1,8 +1,10 @@
-{
+{parameters, ...}: {
   nix = {
     gc.automatic = true;
     settings = {
       auto-optimise-store = true;
+      accept-flake-config = true;
+      trusted-users = ["root" parameters.user];
       experimental-features = ["flakes" "nix-command"];
     };
   };
--- hosts/desktop/nix-settings.nix
+++ hosts/desktop/nix-settings.nix
@@ -1,6 +1,7 @@
 {
   nix.settings = {
     auto-optimise-store = true;
+    accept-flake-config = true;
     trusted-users = ["root" "@wheel"];
     experimental-features = ["flakes" "nix-command"];
   };

864ee2cba87c750903f596c9573098c1758b3313
Pedro Mendes <pedrohrmendes@proton.me>
2024-04-24 22:54:15 -0300

    Update dconf.

--- modules/dconf.nix
+++ modules/dconf.nix
@@ -109,11 +109,21 @@
           toggle-message-tray = ["<Super>n"];
         };
         "org/gnome/desktop/wm/keybindings" = {
+          activate-window-menu = [];
+          begin-move = [];
+          begin-resize = [];
           close = ["<Super>q"];
-          toggle-maximized = ["<Super>z"];
-          toggle-fullscreen = ["<Super>g"];
-          switch-input-source = ["<Alt>space"];
-          switch-input-source-backward = ["<Shift><Alt>space"];
+          cycle-group = [];
+          cycle-group-backward = [];
+          cycle-panels = [];
+          cycle-panels-backward = [];
+          cycle-windows = [];
+          cycle-windows-backward = [];
+          minimize = [];
+          move-to-monitor-down = [];
+          move-to-monitor-left = [];
+          move-to-monitor-right = [];
+          move-to-monitor-up = [];
           move-to-workspace-1 = ["<Shift><Super>1"];
           move-to-workspace-2 = ["<Shift><Super>2"];
           move-to-workspace-3 = ["<Shift><Super>3"];
@@ -123,6 +133,12 @@
           move-to-workspace-7 = ["<Shift><Super>7"];
           move-to-workspace-left = ["<Shift><Control><Super>h"];
           move-to-workspace-right = ["<Shift><Control><Super>l"];
+          switch-group = [];
+          switch-group-backward = [];
+          switch-input-source = ["<Alt>space"];
+          switch-input-source-backward = ["<Shift><Alt>space"];
+          switch-panels = [];
+          switch-panels-backward = [];
           switch-to-workspace-1 = ["<Super>1"];
           switch-to-workspace-2 = ["<Super>2"];
           switch-to-workspace-3 = ["<Super>3"];
@@ -132,6 +148,8 @@
           switch-to-workspace-7 = ["<Super>7"];
           switch-to-workspace-left = ["<Control><Super>h"];
           switch-to-workspace-right = ["<Control><Super>l"];
+          toggle-fullscreen = ["<Super>g"];
+          toggle-maximized = ["<Super>z"];
         };
         "org/gnome/shell/extensions/forge" = {
           move-pointer-focus-enabled = false;

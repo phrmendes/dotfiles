62f1c3f4dbf67c2c7a95a6ae457157be0f7a0450
Pedro Mendes <pedrohrmendes@proton.me>
2024-04-29 19:04:47 -0300

    Change keybindings.
    Change keybindings.

--- dotfiles/nvim/lua/keybindings.lua
+++ dotfiles/nvim/lua/keybindings.lua
@@ -4,228 +4,202 @@ local autocmd = vim.api.nvim_create_autocmd
 local map = vim.keymap.set
 
 vim.g.VM_maps = {
-	["Add Cursor Up"] = "M",
 	["Add Cursor Down"] = "m",
-	["Undo"] = "u",
-	["Redo"] = "<C-r>",
+	["Add Cursor Up"] = "M",
 }
 
-map("n", "<Esc>", "<CMD>nohlsearch<CR>", { noremap = true, silent = true })
-map("n", "<C-d>", "<C-d>zz", { noremap = true, silent = true, desc = "Better page down" })
-map("n", "<C-u>", "<C-u>zz", { noremap = true, silent = true, desc = "Better page up" })
+map("n", "<a-h>", require("smart-splits").swap_buf_left, { desc = "Swap buffer left" })
+map("n", "<a-j>", require("smart-splits").swap_buf_down, { desc = "Swap buffer down" })
+map("n", "<a-k>", require("smart-splits").swap_buf_up, { desc = "Swap buffer up" })
+map("n", "<a-l>", require("smart-splits").swap_buf_right, { desc = "Swap buffer right" })
+map("n", "<c-down>", require("smart-splits").resize_down, { desc = "Resize down" })
+map("n", "<c-left>", require("smart-splits").resize_left, { desc = "Resize left" })
+map("n", "<c-right>", require("smart-splits").resize_right, { desc = "Resize right" })
+map("n", "<c-up>", require("smart-splits").resize_up, { desc = "Resize up" })
+map("n", "<c-d>", "<c-d>zz", { noremap = true, silent = true, desc = "Better page down" })
+map("n", "<c-h>", require("smart-splits").move_cursor_left, { desc = "Move cursor left" })
+map("n", "<c-j>", require("smart-splits").move_cursor_down, { desc = "Move cursor down" })
+map("n", "<c-k>", require("smart-splits").move_cursor_up, { desc = "Move cursor up" })
+map("n", "<c-l>", require("smart-splits").move_cursor_right, { desc = "Move cursor right" })
+map("n", "<c-u>", "<c-u>zz", { noremap = true, silent = true, desc = "Better page up" })
+map("n", "<esc>", "<cmd>nohlsearch<cr>", { noremap = true, silent = true })
+map("n", "Q", "@q", { noremap = true, silent = true })
 map("n", "N", [[v:searchforward ? 'N' : 'n']], { expr = true, noremap = true, silent = true })
 map("n", "n", [[v:searchforward ? 'n' : 'N']], { expr = true, noremap = true, silent = true })
-map("n", "Q", "@q", { noremap = true, silent = true })
 map("n", "j", [[v:count == 0 ? 'gj' : 'j']], { expr = true, noremap = true, silent = true })
 map("n", "k", [[v:count == 0 ? 'gk' : 'k']], { expr = true, noremap = true, silent = true })
-map("n", "<C-h>", require("smart-splits").move_cursor_left, { desc = "Move cursor left" })
-map("n", "<C-j>", require("smart-splits").move_cursor_down, { desc = "Move cursor down" })
-map("n", "<C-k>", require("smart-splits").move_cursor_up, { desc = "Move cursor up" })
-map("n", "<C-l>", require("smart-splits").move_cursor_right, { desc = "Move cursor right" })
-map("n", "<C-Left>", require("smart-splits").resize_left, { desc = "Resize left" })
-map("n", "<C-Down>", require("smart-splits").resize_down, { desc = "Resize down" })
-map("n", "<C-Up>", require("smart-splits").resize_up, { desc = "Resize up" })
-map("n", "<C-Right>", require("smart-splits").resize_right, { desc = "Resize right" })
-map("t", "<C-c>", "<C-\\><C-n>", { noremap = true, silent = true })
-map("v", "Q", "<CMD>norm @q<CR>", { noremap = true, silent = true })
-map({ "n", "v" }, "<C-c><C-c>", "<Plug>SlimeParagraphSend", { noremap = true, silent = true, desc = "Send to REPL" })
-map({ "n", "v" }, "<C-c><C-v>", "<Plug>SlimeConfig", { noremap = true, silent = true, desc = "Config Slime" })
+map("t", "<c-c>", "<c-\\><c-n>", { noremap = true, silent = true })
+map("v", "Q", "<cmd>norm @q<cr>", { noremap = true, silent = true })
+map({ "n", "v" }, "<c-c><c-c>", "<Plug>SlimeParagraphSend", { noremap = true, silent = true, desc = "Send to REPL" })
+map({ "n", "v" }, "<c-c><c-v>", "<Plug>SlimeConfig", { noremap = true, silent = true, desc = "Config Slime" })
 
 wk.register({
-	g = {
-		o = { name = "org" },
-		y = { name = "tables" },
+	["<tab>"] = { mode = "n", name = "tabs" },
+	b = { mode = "n", name = "buffers" },
+	f = { mode = "n", name = "find" },
+	t = { mode = "n", name = "trouble" },
+	z = { mode = "n", name = "zotero" },
+	c = { mode = { "n", "v" }, name = "copilot" },
+	o = { mode = { "n", "v" }, name = "obsidian" },
+	d = {
+		mode = { "n", "v" },
+		name = "DAP",
+		p = { name = "python" },
 	},
+	g = {
+		mode = { "n", "v" },
+		name = "git",
+		s = { name = "stage" },
+		r = { name = "reset" },
+	},
+}, { prefix = "<leader>" })
+
+wk.register({
 	["["] = {
-		name = "previous",
-		["<TAB>"] = { "<CMD>tabprevious<CR>", "Previous tab" },
+		["<tab>"] = { "<cmd>tabprevious<cr>", "Previous tab" },
 		h = { require("gitsigns").prev_hunk, "Previous hunk" },
 		t = { require("todo-comments").jump_prev, "Previous todo" },
 	},
 	["]"] = {
-		name = "next",
-		["<TAB>"] = { "<CMD>tabnext<CR>", "Next tab" },
+		["<tab>"] = { "<cmd>tabnext<cr>", "Next tab" },
 		h = { require("gitsigns").next_hunk, "Next hunk" },
 		t = { require("todo-comments").jump_next, "Next todo" },
 	},
 	["<leader>"] = {
-		["-"] = { "<CMD>split<CR>", "Split window (H)" },
-		["."] = { "<CMD>Telescope commands<CR>", "Commands" },
-		["="] = { "<C-w>=", "Resize and make windows equal" },
-		["?"] = { "<CMD>Telescope help_tags<CR>", "Help" },
-		["\\"] = { "<CMD>vsplit<CR>", "Split window (V)" },
-		["_"] = { "<C-w>_", "Maximize (H)" },
-		["|"] = { "<C-w>|", "Maximize (V)" },
-		E = { "<CMD>NvimTreeToggle<CR>", "File explorer (cwd)" },
-		O = { "<C-w>o", "Keep only current window" },
-		Q = { "<CMD>qall!<CR>", "Quit all" },
-		e = { "<CMD>NvimTreeFindFileToggle<CR>", "File explorer (current file)" },
-		n = { "<CMD>Neogen<CR>", "Generate annotations" },
-		q = { "<CMD>q<CR>", "Quit" },
-		u = { "<CMD>UndotreeToggle<CR>", "Undo tree" },
-		w = { "<CMD>w<CR>", "Save" },
-		x = { "<C-w>q", "Close window" },
-		z = { "<CMD>ZenMode<CR>", "Zen mode" },
+		["-"] = { "<cmd>split<cr>", "Split window (H)" },
+		["."] = { "<cmd>Telescope commands<cr>", "Commands" },
+		["="] = { "<c-w>=", "Resize and make windows equal" },
+		["?"] = { "<cmd>Telescope help_tags<cr>", "Help" },
+		["\\"] = { "<cmd>vsplit<cr>", "Split window (V)" },
+		["_"] = { "<c-w>_", "Maximize (H)" },
+		["|"] = { "<c-w>|", "Maximize (V)" },
+		E = { "<cmd>NvimTreeToggle<cr>", "File explorer (cwd)" },
+		O = { "<c-w>o", "Keep only current window" },
+		Q = { "<cmd>qall!<cr>", "Quit all" },
+		Z = { "<cmd>ZenMode<cr>", "Zen mode" },
+		e = { "<cmd>NvimTreeFindFileToggle<cr>", "File explorer (current file)" },
+		n = { "<cmd>Neogen<cr>", "Generate annotations" },
+		q = { "<cmd>q<cr>", "Quit" },
+		u = { "<cmd>UndotreeToggle<cr>", "Undo tree" },
+		w = { "<cmd>w<cr>", "Save" },
+		x = { "<c-w>q", "Close window" },
 	},
-	["<leader><leader>"] = {
-		name = "local leader",
-		h = { require("smart-splits").swap_buf_left, "Swap buffer left" },
-		j = { require("smart-splits").swap_buf_down, "Swap buffer down" },
-		k = { require("smart-splits").swap_buf_up, "Swap buffer up" },
-		l = { require("smart-splits").swap_buf_right, "Swap buffer right" },
-		p = { "<CMD>PasteImage<CR>", "Paste image" },
-		u = { "<CMD>MkdnUpdateNumbering<CR>", "Update numbering" },
-		f = { "<CMD>MkdnFoldSection<CR>", "Fold section" },
-		F = { "<CMD>MkdnUnfoldSection<CR>", "Unfold section" },
-	},
-	["<leader><TAB>"] = {
-		name = "tabs",
-		["<TAB>"] = { "<CMD>tab split<CR>", "Open in new tab" },
-		G = { "<CMD>tablast<CR>", "Last tab" },
-		d = { "<CMD>tabclose<CR>", "Close tab" },
-		g = { "<CMD>tabfirst<CR>", "First tab" },
-		k = { "<CMD>tabonly<CR>", "Keep only this tab" },
-		n = { "<CMD>tabnew<CR>", "New tab" },
+	["<leader><tab>"] = {
+		["<tab>"] = { "<cmd>tab split<cr>", "Open in new tab" },
+		G = { "<cmd>tablast<cr>", "Last tab" },
+		d = { "<cmd>tabclose<cr>", "Close tab" },
+		g = { "<cmd>tabfirst<cr>", "First tab" },
+		k = { "<cmd>tabonly<cr>", "Keep only this tab" },
+		n = { "<cmd>tabnew<cr>", "New tab" },
 	},
 	["<leader>b"] = {
-		name = "buffers",
-		G = { "<CMD>blast<CR>", "Go to last buffer" },
-		b = { "<CMD>Telescope buffers<CR>", "List" },
+		G = { "<cmd>blast<cr>", "Go to last buffer" },
+		b = { "<cmd>Telescope buffers<cr>", "List" },
 		d = { require("mini.bufremove").delete, "Delete" },
-		f = { "<CMD>Telescope current_buffer_fuzzy_find<CR>", "Search in current buffer" },
-		g = { "<CMD>bfirst<CR>", "Go to last buffer" },
-		k = { "<CMD>%bdelete<bar>edit#<bar>bdelete#<CR>", "Keep only this buffer" },
+		f = { "<cmd>Telescope current_buffer_fuzzy_find<cr>", "Search in current buffer" },
+		g = { "<cmd>bfirst<cr>", "Go to last buffer" },
+		k = { "<cmd>%bdelete<bar>edit#<bar>bdelete#<cr>", "Keep only this buffer" },
 		w = { require("mini.bufremove").wipeout, "Wipeout" },
 	},
 	["<leader>c"] = {
-		name = "copilot",
-		c = { "<CMD>CopilotChatToggle<CR>", "Open" },
-		r = { "<CMD>CopilotChatReset<CR>", "Reset" },
+		c = { "<cmd>CopilotChatToggle<cr>", "Open" },
+		r = { "<cmd>CopilotChatReset<cr>", "Reset" },
+		d = { mode = "v", "<cmd>CopilotChatDocs<cr>", "Add documentation" },
+		e = { mode = "v", "<cmd>CopilotChatExplain<cr>", "Explain code" },
+		f = { mode = "v", "<cmd>CopilotChatFix<cr>", "Fix code" },
+		o = { mode = "v", "<cmd>CopilotChatOptimize<cr>", "Optimize code" },
+		t = { mode = "v", "<cmd>CopilotChatTests<cr>", "Generate tests" },
 	},
 	["<leader>f"] = {
-		name = "find",
-		f = { "<CMD>Telescope find_files<CR>", "Files" },
-		g = { "<CMD>Telescope live_grep<CR>", "Live grep" },
-		o = { "<CMD>Telescope oldfiles<CR>", "Recent files" },
+		f = { "<cmd>Telescope find_files<cr>", "Files" },
+		g = { "<cmd>Telescope live_grep<cr>", "Live grep" },
+		o = { "<cmd>Telescope oldfiles<cr>", "Recent files" },
 		r = { require("spectre").toggle, "Replace" },
-		t = { "<CMD>TodoTelescope<CR>", "Todo" },
-		z = { "<CMD>Telescope zoxide list<CR>", "Zoxide" },
+		t = { "<cmd>TodoTelescope<cr>", "Todo" },
+		z = { "<cmd>Telescope zoxide list<cr>", "Zoxide" },
 	},
 	["<leader>g"] = {
-		name = "git",
 		B = { require("gitsigns").toggle_current_line_blame, "Blame line" },
-		C = { "<CMD>Telescope git_commits<CR>", "Commits (cwd)" },
-		G = { "<CMD>LazyGit<CR>", "LazyGit" },
-		b = { "<CMD>Telescope git_branches<CR>", "Checkout" },
-		c = { "<CMD>Telescope git_bcommits<CR>", "Commits (current file)" },
+		C = { "<cmd>Telescope git_commits<cr>", "Commits (cwd)" },
+		G = { "<cmd>LazyGit<cr>", "LazyGit" },
+		b = { "<cmd>Telescope git_branches<cr>", "Checkout" },
+		c = { "<cmd>Telescope git_bcommits<cr>", "Commits (current file)" },
 		d = { require("gitsigns").diffthis, "Diff" },
-		f = { "<CMD>Telescope git_files<CR>", "Files" },
-		g = { "<CMD>LazyGitCurrentFile<CR>", "LazyGit (current file)" },
+		f = { "<cmd>Telescope git_files<cr>", "Files" },
+		g = { "<cmd>LazyGitCurrentFile<cr>", "LazyGit (current file)" },
 		s = {
-			name = "stage",
-			h = { require("gitsigns").stage_hunk, "Hunk" },
+			h = { mode = { "n", "v" }, require("gitsigns").stage_hunk, "Hunk" },
 			b = { require("gitsigns").stage_buffer, "Buffer" },
 		},
 		r = {
-			name = "reset",
-			h = { require("gitsigns").reset_hunk, "Hunk" },
+			h = { mode = { "n", "v" }, require("gitsigns").reset_hunk, "Hunk" },
 			b = { require("gitsigns").reset_buffer, "Buffer" },
 		},
 	},
 	["<leader>o"] = {
-		name = "obsidian",
-		b = { "<CMD>ObsidianBacklinks<CR>", "Backlinks" },
-		n = { "<CMD>ObsidianNew<CR>", "New note" },
-		o = { "<CMD>ObsidianQuickSwitch<CR>", "Search notes" },
-		p = { "<CMD>ObsidianPasteImg<CR>", "Paste image" },
-		r = { "<CMD>ObsidianRename<CR>", "Rename note" },
-		s = { "<CMD>ObsidianSearch<CR>", "Search in notes" },
-		t = { "<CMD>ObsidianTags<CR>", "Tags" },
+		b = { "<cmd>ObsidianBacklinks<cr>", "Backlinks" },
+		c = { "<cmd>ObsidianNew<cr>", "Create note" },
+		e = { mode = "v", "<cmd>ObsidianExtractNote<cr>", "Extract to new note" },
+		l = { mode = "v", "<cmd>ObsidianLink<cr>", "Add link" },
+		n = { mode = "v", "<cmd>ObsidianLinkNew<cr>", "Add link to new file" },
+		o = { "<cmd>ObsidianQuickSwitch<cr>", "Search notes" },
+		p = { "<cmd>ObsidianPasteImg<cr>", "Paste image" },
+		r = { "<cmd>ObsidianRename<cr>", "Rename note" },
+		s = { "<cmd>ObsidianSearch<cr>", "Search in notes" },
+		t = { "<cmd>ObsidianTags<cr>", "Tags" },
+	},
+	["<leader>t"] = {
+		T = { "<cmd>TodoTrouble", "Todo" },
+		r = { "<cmd>TroubleRefresh<cr>", "Refresh" },
+		t = { "<cmd>TroubleToggle<cr>", "Toggle" },
 	},
 })
 
-wk.register({
-	["<leader>c"] = {
-		name = "copilot",
-		d = { "<CMD>CopilotChatDocs<CR>", "Add documentation" },
-		e = { "<CMD>CopilotChatExplain<CR>", "Explain code" },
-		f = { "<CMD>CopilotChatFix<CR>", "Fix code" },
-		o = { "<CMD>CopilotChatOptimize<CR>", "Optimize code" },
-		t = { "<CMD>CopilotChatTests<CR>", "Generate tests" },
-	},
-	["<leader>g"] = {
-		name = "git",
-		s = {
-			name = "stage",
-			h = { require("gitsigns").stage_hunk, "Hunk" },
-		},
-		r = {
-			name = "reset",
-			h = { require("gitsigns").reset_hunk, "Hunk" },
-		},
-	},
-	["<leader>o"] = {
-		name = "obsidian",
-		e = { "<CMD>ObsidianExtractNote<CR>", "Extract to new note" },
-		l = { "<CMD>ObsidianLink<CR>", "Add link" },
-		n = { "<CMD>ObsidianLinkNew<CR>", "Add link to new file" },
-	},
-}, { mode = "v" })
-
 autocmd("FileType", {
-	pattern = "quarto",
 	group = augroup,
-	callback = function(event)
-		wk.register({
-			["<leader>z"] = { "<CMD>Telescope zotero<CR>", "Add source from Zotero" },
-		}, { buffer = event.buf })
-	end,
-})
-
-autocmd("FileType", {
-	pattern = { "markdown", "quarto" },
-	group = augroup,
-	callback = function(event)
+	pattern = "markdown",
+	callback = function(ev)
 		wk.register({
 			["<leader>z"] = {
-				name = "zotero",
 				c = { "<Plug>ZCitationCompleteInfo", "Citation complete info" },
 				i = { "<Plug>ZCitationInfo", "Citation info" },
 				o = { "<Plug>ZOpenAttachment", "Open attachment" },
 				v = { "<Plug>ZViewDocument", "View document" },
 				y = { "<Plug>ZCitationYamlRef", "YAML reference" },
 			},
-		}, { buffer = event.buf })
+		}, { buffer = ev.buf })
 	end,
 })
 
 autocmd("FileType", {
-	pattern = "python",
 	group = augroup,
-	callback = function(event)
+	pattern = "quarto",
+	callback = function(ev)
 		wk.register({
-			["<leader>t"] = {
-				name = "DAP",
-				t = { require("dap-python").test_file, "Test file (python)" },
-				d = { require("dap-python").debug_file, "Debug file (python)" },
+			["<leader>z"] = {
+				z = { "<cmd>Telescope zotero<cr>", "Add source from Zotero" },
 			},
-		}, { buffer = event.buf })
+		}, { buffer = ev.buf })
+	end,
+})
 
+autocmd("FileType", {
+	group = augroup,
+	pattern = "python",
+	callback = function(ev)
 		wk.register({
-			["<leader>t"] = {
-				name = "DAP",
-				d = { require("dap-python").debug_selection, "Debug selection (python)" },
+			["<leader>dp"] = {
+				d = { require("dap-python").debug_file, "Debug file" },
+				s = { mode = "v", require("dap-python").debug_selection, "Debug selection" },
+				t = { require("dap-python").test_file, "Test file" },
 			},
-		}, {
-			mode = "v",
-			buffer = event.buf,
-		})
+		}, { buffer = ev.buf })
 	end,
 })
 
 autocmd("LspAttach", {
 	group = augroup,
-	callback = function(event)
+	callback = function(ev)
 		wk.register({
 			["<F3>"] = { require("dap").step_out, "DAP: step out" },
 			["<F4>"] = { require("dap").step_into, "DAP: step into" },
@@ -233,42 +207,49 @@ autocmd("LspAttach", {
 			["<F6>"] = { require("dap").continue, "DAP: continue" },
 			["<F7>"] = { require("dap").step_over, "DAP: step over" },
 			["<S-F6>"] = { require("dap").pause, "DAP: pause" },
-			["<BS>"] = { require("dap").close, "DAP: quit" },
-			["g"] = {
+			["<bs>"] = { require("dap").close, "DAP: quit" },
+			g = {
 				D = { vim.lsp.buf.declaration, "Go to declaration" },
-				d = { "<CMD>Telescope lsp_definitions<CR>", "Go to definition" },
-				i = { "<CMD>Telescope lsp_implementations<CR>", "Go to implementations" },
-				t = { "<CMD>Telescope lsp_type_definitions<CR>", "Go to type definition" },
+				d = { "<cmd>Trouble lsp_definitions<cr>", "Go to definition" },
+				i = { "<cmd>Trouble lsp_implementations<cr>", "Go to implementations" },
+				t = { "<cmd>Trouble lsp_type_definitions<cr>", "Go to type definition" },
 			},
 			["<leader>"] = {
-				k = { vim.lsp.buf.hover, "Show hover documentation" },
-				r = { vim.lsp.buf.rename, "Rename symbol" },
-				D = { "<CMD>TroubleToggle workspace_diagnostics<CR>", "Workspace diagnostics" },
 				F = { vim.diagnostic.open_float, "Floating diagnostics" },
-				R = { "<CMD>Telescope lsp_references<CR>", "Show references" },
-				S = { "<CMD>Telescope lsp_workspace_symbols<CR>", "Workspace symbols" },
-				d = { "<CMD>TroubleToggle document_diagnostics<CR>", "Document diagnostics" },
+				R = { "<cmd>Trouble lsp_references<cr>", "Show references" },
+				S = { "<cmd>Telescope lsp_workspace_symbols<cr>", "Workspace symbols" },
+				a = { mode = { "n", "v" }, require("actions-preview").code_actions, "Code actions" },
 				h = { require("lsp_signature").toggle_float_win, "Toggle signature help" },
-				s = { "<CMD>Telescope lsp_document_symbols<CR>", "Document symbols" },
-			},
-			["<leader>t"] = {
-				name = "DAP",
-				B = {
-					function()
-						require("dap").set_breakpoint(vim.fn.input("Breakpoint condition: "))
-					end,
-					"Toggle conditional breakpoint",
+				k = { vim.lsp.buf.hover, "Show hover" },
+				r = { vim.lsp.buf.rename, "Rename symbol" },
+				s = { "<cmd>Telescope lsp_document_symbols<cr>", "Document symbols" },
+				t = {
+					name = "trouble",
+					d = { "<cmd>TroubleToggle document_diagnostics<cr>", "Document diagnostics" },
+					w = { "<cmd>TroubleToggle workspace_diagnostics<cr>", "Workspace diagnostics" },
 				},
-				b = { require("dap").toggle_breakpoint, "Toggle breakpoint" },
-				u = { require("dapui").toggle, "Toggle UI" },
+				d = {
+					b = { require("dap").toggle_breakpoint, "Breakpoint" },
+					c = { require("dap").clear_breakpoints, "Clear all breakpoints" },
+					k = { require("dap.ui.widgets").hover, "Show hover" },
+					l = { "<cmd>Telescope dap list_breakpoints<cr>", "List breakpoints" },
+					r = { require("dap").repl.toggle, "Toggle REPL" },
+					u = { require("dapui").toggle, "Toggle UI" },
+					q = {
+						function()
+							require("dap").terminate()
+							require("dapui").close()
+						end,
+						"Terminate",
+					},
+					B = {
+						function()
+							require("dap").set_breakpoint(vim.fn.input("Breakpoint condition: "))
+						end,
+						"Conditional breakpoint",
+					},
+				},
 			},
-		}, { buffer = event.buf })
-
-		wk.register({
-			["<leader>a"] = { require("actions-preview").code_actions, "Code actions" },
-		}, {
-			mode = "v",
-			buffer = event.buf,
-		})
+		}, { buffer = ev.buf })
 	end,
 })

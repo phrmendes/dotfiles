62a4cdad61a9b28b5a772b20c14484fbad457b8f
Pedro Mendes <pedro.mendes-ext@ab-inbev.com>
2024-05-20 16:57:39 -0300

    Update darwin options.

--- hosts/shared/stylix.nix
+++ hosts/shared/stylix.nix
@@ -1,37 +1,46 @@
-{pkgs, ...}: {
-  stylix = {
-    image = ../../dotfiles/background.png;
-    base16Scheme = "${pkgs.base16-schemes}/share/themes/gruvbox-material-dark-hard.yaml";
-    polarity = "dark";
-    cursor = {
-      name = "Pop";
-      package = pkgs.pop-icon-theme;
-      size = 28;
+{
+  pkgs,
+  lib,
+  ...
+}: {
+  stylix = let
+    inherit (pkgs.stdenv) isLinux;
+  in
+    {
+      image = ../../dotfiles/background.png;
+      base16Scheme = "${pkgs.base16-schemes}/share/themes/gruvbox-material-dark-hard.yaml";
+      polarity = "dark";
+      fonts = {
+        sizes = {
+          applications = 12;
+          terminal =
+            if isLinux
+            then 12
+            else 16;
+        };
+        serif = {
+          package = pkgs.fira;
+          name = "Fira Sans";
+        };
+        sansSerif = {
+          package = pkgs.fira;
+          name = "Fira Sans";
+        };
+        monospace = {
+          package = pkgs.fira-code-nerdfont;
+          name = "FiraCode Nerd Font Mono";
+        };
+        emoji = {
+          package = pkgs.noto-fonts-emoji;
+          name = "Noto Color Emoji";
+        };
+      };
+    }
+    // lib.optionalAttrs isLinux {
+      cursor = {
+        name = "Pop";
+        package = pkgs.pop-icon-theme;
+        size = 28;
+      };
     };
-    fonts = {
-      sizes = {
-        applications = 12;
-        terminal =
-          if pkgs.stdenv.isDarwin
-          then 16
-          else 12;
-      };
-      serif = {
-        package = pkgs.fira;
-        name = "Fira Sans";
-      };
-      sansSerif = {
-        package = pkgs.fira;
-        name = "Fira Sans";
-      };
-      monospace = {
-        package = pkgs.fira-code-nerdfont;
-        name = "FiraCode Nerd Font Mono";
-      };
-      emoji = {
-        package = pkgs.noto-fonts-emoji;
-        name = "Noto Color Emoji";
-      };
-    };
-  };
 }

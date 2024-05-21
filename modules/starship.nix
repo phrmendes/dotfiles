30716dbd5bae252ad88231f0348b300b1209ac84
Pedro Mendes <pedrohrmendes@proton.me>
2024-05-21 19:07:01 -0300

    Update starship.

--- modules/starship.nix
+++ modules/starship.nix
@@ -9,6 +9,7 @@
     programs.starship = {
       enable = true;
       enableZshIntegration = true;
+      enableBashIntegration = true;
       settings = {
         command_timeout = 1000;
         docker_context.disabled = true;

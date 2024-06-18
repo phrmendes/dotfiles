7224e2362378d60c37fdea457e11eae4ab85933c
Pedro Mendes <pedrohrmendes@proton.me>
2024-06-18 00:01:42 -0300

    Update kitty padding.

--- modules/kitty.nix
+++ modules/kitty.nix
@@ -30,7 +30,7 @@
         tab_title_template = "{title}{' #{}'.format(num_windows) if num_windows > 1 else ''}";
         undercurl_style = "thin-sparse";
         update_check_interval = 0;
-        window_padding_width = 8;
+        window_padding_width = 6;
         term = "xterm-256color";
       };
       keybindings = {

90a9366ed058e4793cdb23ce9876a75f3cc2a5d5
Pedro Mendes <pedrohrmendes@proton.me>
2024-06-18 12:14:37 -0300

    Set stylix branch.

--- flake.nix
+++ flake.nix
@@ -16,7 +16,7 @@
     impermanence.url = "github:nix-community/impermanence";
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-24.05";
-    stylix.url = "github:danth/stylix";
+    stylix.url = "github:danth/stylix/release-23.11";
     walker.url = "github:abenz1267/walker";
 
     darwin = {

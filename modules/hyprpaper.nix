700191fd258b8e4b1e2d84596a9ad07a4b61bfb3
Pedro Mendes <pedrohrmendes@proton.me>
2024-06-20 21:15:50 -0300

    Change hyprpaper settings.

--- modules/hyprpaper.nix
+++ modules/hyprpaper.nix
@@ -6,12 +6,6 @@
   options.hyprpaper.enable = lib.mkEnableOption "enable hyprpaper";
 
   config = lib.mkIf config.hyprpaper.enable {
-    services.hyprpaper = {
-      enable = true;
-      settings = {
-        preload = "${../dotfiles/background.png}";
-        wallpaper = ",${../dotfiles/background.png}";
-      };
-    };
+    services.hyprpaper.enable = true;
   };
 }

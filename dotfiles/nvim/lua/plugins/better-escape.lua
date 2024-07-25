e646e28ed07582ed00e9f691f0c35c753edddfe1
Pedro Mendes <pedrohrmendes@proton.me>
2024-07-25 15:37:31 -0300

    Update better-escape mappings.

--- dotfiles/nvim/lua/plugins/better-escape.lua
+++ dotfiles/nvim/lua/plugins/better-escape.lua
@@ -1,1 +1,30 @@
-require("better_escape").setup()
+require("better_escape").setup({
+	default_mappings = false,
+	mappings = {
+		i = {
+			j = {
+				k = "<Esc>",
+			},
+		},
+		c = {
+			j = {
+				k = "<Esc>",
+			},
+		},
+		t = {
+			["<c-c>"] = {
+				["<c-c>"] = "<Esc>",
+			},
+		},
+		v = {
+			j = {
+				k = "<Esc>",
+			},
+		},
+		s = {
+			j = {
+				k = "<Esc>",
+			},
+		},
+	},
+})

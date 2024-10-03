4d1aea8f7a5291e122bb6f9201555bd64e21de3f
Pedro Mendes <pedrohrmendes@proton.me>
2024-10-03 17:14:37 -0300

    Using natural scroll.

--- modules/hyprland.nix
+++ modules/hyprland.nix
@@ -68,6 +68,7 @@
           numlock_by_default = true;
           follow_mouse = 1;
           sensitivity = 0;
+          touchpad.natural_scroll = true;
         };
         general = {
           gaps_in = 3;

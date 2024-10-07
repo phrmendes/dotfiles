577aaf5390db710b9870cafb60a565f88b441f2b
Pedro Mendes <pedrohrmendes@proton.me>
2024-10-07 12:19:44 -0300

    Add mongo helpers.

--- dotfiles/nvim/lua/plugins/dadbod.lua
+++ dotfiles/nvim/lua/plugins/dadbod.lua
@@ -1,3 +1,16 @@
 vim.g.db_ui_env_variable_url = "DB_CONNECTION_STRING"
 vim.g.db_ui_use_nvim_notify = 1
 vim.g.db_ui_use_nerd_fonts = 1
+vim.g.db_ui_table_helpers = {
+	mongodb = {
+		Aggregate = "{table}.aggregate([])",
+		Count = "{table}.countDocuments()",
+		Distinct = "{table}.distinct()",
+		EstimatedDocumentCount = "{table}.estimatedDocumentCount()",
+		Find = "{table}.find()",
+		FindOne = "{table}.findOne()",
+		Insert = "{table}.insertOne()",
+		InsertMany = "{table}.insertMany([])",
+		MapReduce = "{table}.mapReduce()",
+	},
+}

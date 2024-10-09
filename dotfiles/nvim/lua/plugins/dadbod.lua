f978063be622885700c55deefe06dfeda9db43b8
Pedro Mendes <pedrohrmendes@proton.me>
2024-10-08 21:20:45 -0300

    Update dadbod options.
    .

--- dotfiles/nvim/lua/plugins/dadbod.lua
+++ dotfiles/nvim/lua/plugins/dadbod.lua
@@ -1,4 +1,5 @@
-vim.g.db_ui_env_variable_url = "DB_CONNECTION_STRING"
+vim.g.db_ui_env_variable_url = "DB_URL"
+vim.g.db_ui_env_variable_name = "DB_NAME"
 vim.g.db_ui_use_nvim_notify = 1
 vim.g.db_ui_use_nerd_fonts = 1
 vim.g.db_ui_table_helpers = {

bdda142adc22336dfeb964b8e1397d85bb317f37
Pedro Mendes <pedrohrmendes@proton.me>
2024-11-21 13:23:06 -0300

    Add wezterm.

--- dotfiles/nvim/lua/plugins/tests.lua
+++ dotfiles/nvim/lua/plugins/tests.lua
@@ -1,4 +1,9 @@
-vim.cmd([[let test#strategy = 'vimux']])
+if vim.env.TMUX then
+	vim.cmd([[let test#strategy = 'vimux']])
+else
+	vim.cmd([[let test#strategy = 'wezterm']])
+end
+
 vim.cmd([[let test#python#runner = 'pytest']])
 vim.cmd([[let test#elixir#runner = 'exunit']])
 vim.cmd([[let test#go#runner = 'gotest']])

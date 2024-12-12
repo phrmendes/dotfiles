5286a6efc342e51af90098bef46f7861ef1b7e95
Pedro Mendes <pedrohrmendes@proton.me>
2024-12-12 12:28:23 -0300

    Removing wezterm.

--- dotfiles/nvim/lua/plugins/slime.lua
+++ dotfiles/nvim/lua/plugins/slime.lua
@@ -1,14 +1,4 @@
-local config, target
-
-if vim.env.TMUX then
-	config = { socket_name = "default", target_pane = "{last}" }
-	target = "tmux"
-else
-	config = { pane_direction = "right" }
-	target = "wezterm"
-end
-
 vim.g.slime_bracketed_paste = 1
-vim.g.slime_default_config = config
+vim.g.slime_default_config = { socket_name = "default", target_pane = "{last}" }
 vim.g.slime_no_mappings = true
-vim.g.slime_target = target
+vim.g.slime_target = "tmux"

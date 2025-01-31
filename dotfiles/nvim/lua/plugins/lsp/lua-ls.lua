3ff87529b009afb99aad78c632bcdb2840cb5355
Pedro Mendes <pedrohrmendes@proton.me>
2025-01-30 23:15:11 -0300

    Add global variable to list.

--- dotfiles/nvim/lua/plugins/lsp/lua-ls.lua
+++ dotfiles/nvim/lua/plugins/lsp/lua-ls.lua
@@ -3,7 +3,7 @@ return {
 		Lua = {
 			completion = { callSnippet = "Replace" },
 			diagnostics = {
-				globals = { "vim", "_" },
+				globals = { "vim", "_", "_G" },
 				disable = { "missing-fields" },
 			},
 			telemetry = { enable = false },

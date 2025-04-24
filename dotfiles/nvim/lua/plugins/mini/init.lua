ae262d50fd68452b70689c4fe86163e4746c2e99
Pedro Mendes <pedrohrmendes@proton.me>
2025-04-24 12:27:49 -0300

    Update keybindings.

--- dotfiles/nvim/lua/plugins/mini/init.lua
+++ dotfiles/nvim/lua/plugins/mini/init.lua
@@ -40,11 +40,7 @@ return {
 		{ "<leader>gc", "<cmd>Git commit<cr>", desc = "Commit" },
 		{ "<leader>gp", "<cmd>Git pull<cr>", desc = "Pull" },
 		{ "<leader>gP", "<cmd>Git push<cr>", desc = "Push" },
-		{
-			"<localleader>d",
-			function() require("mini.doc").generate() end,
-			desc = "mini: generate lua documentation",
-			ft = "lua",
-		},
+		{ "<localleader>d", function() require("mini.doc").generate() end, desc = "mini: generate docs", ft = "lua" },
+		{ "<localleader>t", function() require("mini.test").run() end, desc = "mini: run tests", ft = "lua" },
 	},
 }

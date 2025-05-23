dadb9f32cd040bc04fe50067f3867876ab596926
Pedro Mendes <pedrohrmendes@proton.me>
2025-05-22 23:37:38 -0300

    Update starship options.

--- modules/starship.nix
+++ modules/starship.nix
@@ -11,7 +11,18 @@
       enable = true;
       enableZshIntegration = true;
       enableBashIntegration = true;
-      settings = builtins.fromTOML (builtins.readFile ../dotfiles/starship.toml);
+      settings = {
+        command_timeout = 1000;
+        docker_context.disabled = true;
+        gcloud.disabled = true;
+        nix_shell.symbol = " ";
+        python.symbol = " ";
+        character = {
+          success_symbol = "[󰘧](bold green)";
+          error_symbol = "[󰘧](bold red)";
+          vimcmd_symbol = "[󰘧](bold purple)";
+        };
+      };
     };
   };
 }

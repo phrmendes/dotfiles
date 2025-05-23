f0b0ccefe79c6efd568e1a903ab98366e6ac9811
Pedro Mendes <pedrohrmendes@proton.me>
2025-05-23 13:21:43 -0300

    Update copilot settings.

--- dotfiles/nvim/lua/plugins/copilot.lua
+++ dotfiles/nvim/lua/plugins/copilot.lua
@@ -1,33 +1,47 @@
-local prompts = {
-	Concise = "Rewrite the following text to make it more concise.",
-	Documentation = "Provide documentation for the following code.",
-	Spelling = "Correct and improve any grammar or spelling errors in the following text.",
-	Summarize = "Summarize the following text.",
-}
-
 return {
-	{ "github/copilot.vim", event = "InsertEnter" },
+	{
+		"github/copilot.vim",
+		event = "InsertEnter",
+		init = function() vim.g.copilot_filetypes = { ["copilot-chat"] = false } end,
+	},
 	{
 		"CopilotC-Nvim/CopilotChat.nvim",
 		dependencies = {
 			{ "github/copilot.vim" },
 			{ "nvim-lua/plenary.nvim" },
 		},
-		init = function()
-			vim.g.copilot_filetypes = {
-				["copilot-chat"] = false,
-			}
-		end,
 		cmd = { "CopilotChat", "CopilotChatToggle" },
 		opts = {
 			chat_autocomplete = true,
-			prompts = prompts,
+			selection = function(source) return require("CopilotChat.select").visual(source) end,
+			question_header = "# User ",
+			answer_header = "# Copilot ",
+			error_header = "# Error ",
+			separator = "───",
 			mappings = {
 				submit_prompt = { normal = "<cr>", insert = "<c-s>" },
 				reset = { normal = "<leader><bs>", insert = "<c-r>" },
 			},
-			selection = function(source) return require("CopilotChat.select").visual(source) end,
+			prompts = {
+				Concise = "Rewrite the following text to make it more concise:\n",
+				Documentation = "Provide documentation for the following code:\n",
+				Spelling = "Correct and improve any grammar or spelling errors in the following text:\n",
+				Summarize = "Summarize the following text:\n",
+			},
 		},
+		config = function(_, opts)
+			require("CopilotChat").setup(opts)
+
+			vim.api.nvim_create_autocmd("BufEnter", {
+				desc = "Options for copilot filetypes",
+				group = vim.api.nvim_create_augroup("UserCopilotFiletype", { clear = true }),
+				pattern = "copilot-*",
+				callback = function()
+					vim.opt_local.relativenumber = false
+					vim.opt_local.number = false
+				end,
+			})
+		end,
 		keys = {
 			{ "<leader>c", "", desc = "+copilot", mode = { "n", "x" } },
 			{ "<leader>cc", "<cmd>CopilotChat<cr>", mode = "x", desc = "Chat" },

c0e90e41fff31c14b1f2d4311e0aa7a9a6e43817
Pedro Mendes <pedrohrmendes@proton.me>
2025-05-24 11:53:03 -0300

    Update kulala options.

--- dotfiles/nvim/lua/plugins/kulala.lua
+++ dotfiles/nvim/lua/plugins/kulala.lua
@@ -1,53 +1,47 @@
+local ft = { "http", "rest" }
+
 return {
 	"mistweaverco/kulala.nvim",
-	ft = "http",
+	ft = ft,
 	opts = {
-		icons = {
-			inlay = {
-				done = "",
-				error = "󰅚",
-				loading = "",
+		ui = {
+			formatter = true,
+			icons = {
+				inlay = {
+					done = "",
+					error = "󰅚",
+					loading = "",
+				},
 			},
 		},
 		kulala_keymaps = {
 			["show headers"] = { "H", function() require("kulala.ui").show_headers() end },
 		},
-		contenttypes = {
-			["application/json"] = {
-				ft = "json",
-			},
-			["application/xml"] = {
-				ft = "xml",
-			},
-			["text/html"] = {
-				ft = "html",
-			},
-		},
 	},
 	keys = {
-		{ "<localleader>k", "", desc = "+kulala" },
-		{ "<localleader>ks", function() require("kulala").scratchpad() end, desc = "Scratchpad" },
-		{ "<localleader>kS", function() require("kulala").show_stats() end, desc = "Statistics", ft = "http" },
-		{ "<localleader>kc", function() require("kulala").copy() end, desc = "Copy as a curl command", ft = "http" },
-		{ "<localleader>ki", function() require("kulala").inspect() end, desc = "Inspect", ft = "http" },
-		{ "<localleader>kr", function() require("kulala").replay() end, desc = "Replay", ft = "http" },
-		{ "<localleader>kq", function() require("kulala").close() end, desc = "Close", ft = "http" },
-		{ "<localleader>kk", function() require("kulala").run() end, mode = { "n", "x" }, desc = "Send", ft = "http" },
+		{ "<leader>k", "", desc = "+kulala" },
+		{ "<leader>ks", function() require("kulala").scratchpad() end, desc = "Scratchpad" },
+		{ "<leader>kS", function() require("kulala").show_stats() end, desc = "Statistics", ft = ft },
+		{ "<leader>kc", function() require("kulala").copy() end, desc = "Copy as a curl command", ft = ft },
+		{ "<leader>ki", function() require("kulala").inspect() end, desc = "Inspect", ft = ft },
+		{ "<leader>kr", function() require("kulala").replay() end, desc = "Replay", ft = ft },
+		{ "<leader>kq", function() require("kulala").close() end, desc = "Close", ft = ft },
+		{ "<leader>kk", function() require("kulala").run() end, mode = { "n", "x" }, desc = "Send", ft = ft },
 		{
-			"<localleader>kf",
+			"<leader>kf",
 			function() require("kulala").from_curl() end,
 			desc = "Paste curl from clipboard",
 			ft = "http",
 		},
 		{
-			"<localleader>ka",
+			"<leader>ka",
 			function() require("kulala").run_all() end,
 			desc = "Send all",
 			ft = "http",
 			mode = { "n", "x" },
 		},
 		{
-			"<localleader>kt",
+			"<leader>kt",
 			function() require("kulala").toggle_view() end,
 			desc = "Toggle between body and headers",
 			ft = "http",

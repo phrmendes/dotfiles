d3c7db17c0c4206b1c2418d8dad9fa94e5d014fc
Pedro Mendes <pedrohrmendes@proton.me>
2025-06-18 13:20:11 -0300

    Add restart systemd services.

--- hosts/server/systemd.nix
+++ hosts/server/systemd.nix
@@ -1,16 +1,35 @@
 { pkgs, ... }:
 {
-  systemd.services.docker-compose = {
-    description = "Docker Compose Service";
-    after = [ "docker.service" ];
-    requires = [ "docker.service" ];
-    wantedBy = [ "multi-user.target" ];
-    serviceConfig = {
-      WorkingDirectory = "/etc/compose";
-      ExecStart = "${pkgs.docker}/bin/docker compose up --detach --remove-orphans";
-      ExecStop = "${pkgs.docker}/bin/docker compose down";
-      Restart = "always";
-      RemainAfterExit = true;
+  systemd = {
+    paths = {
+      docker-compose-reload = {
+        description = "Watch for changes in Docker Compose configuration";
+        pathConfig.PathChanged = "/etc/compose/docker-compose.yaml";
+        wantedBy = [ "multi-user.target" ];
+      };
+    };
+    services = {
+      docker-compose = {
+        description = "Docker Compose service";
+        after = [ "docker.service" ];
+        requires = [ "docker.service" ];
+        wantedBy = [ "multi-user.target" ];
+        serviceConfig = {
+          WorkingDirectory = "/etc/compose";
+          ExecStart = "${pkgs.docker}/bin/docker compose up --detach --remove-orphans";
+          ExecStop = "${pkgs.docker}/bin/docker compose down";
+          Restart = "always";
+          RemainAfterExit = true;
+        };
+      };
+      docker-compose-reload = {
+        description = "Reload Docker Compose on config change";
+        serviceConfig = {
+          Type = "oneshot";
+          ExecStart = "${pkgs.docker}/bin/docker compose restart";
+          WorkingDirectory = "/etc/compose";
+        };
+      };
     };
   };
 }

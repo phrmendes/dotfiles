b67b4c5dc1546dd9ee094b95638446a2898ab6fe
Pedro Mendes <pedrohrmendes@proton.me>
2025-07-02 13:45:16 -0300

    Update systemd options.

--- hosts/server/systemd.nix
+++ hosts/server/systemd.nix
@@ -14,40 +14,40 @@
     };
     services = {
       nixos-rebuild-switch = {
-        description = "Rebuild NixOS configuration";
+        description = "NixOS rebuild switch service";
         serviceConfig = {
           Type = "oneshot";
-          ExecStart = "${pkgs.nixos-rebuild}/bin/nixos-rebuild switch --flake ${parameters.home}/dotfiles#${config.networking.hostName}";
           RemainAfterExit = true;
+          WorkingDirectory = "${parameters.home}/dotfiles/compose";
+          StandardOutput = "journal";
+          StandardError = "journal";
+          ExecStart = ''
+            ${pkgs.nixos-rebuild}/bin/nixos-rebuild switch --flake ${parameters.home}/dotfiles#${config.networking.hostName}
+          '';
         };
       };
       docker-compose = {
-        description = "Start, stop, and reload Docker Compose services";
-        requires = [
-          "chown-volumes.service"
-          "nixos-rebuild-switch.service"
-        ];
-        after = [
-          "chown-volumes.service"
-          "nixos-rebuild-switch.service"
-        ];
+        description = "Docker compose systemd service";
+        after = [ "nixos-rebuild-switch.service" ];
+        requires = [ "nixos-rebuild-switch.service" ];
         serviceConfig = {
           Type = "oneshot";
+          RemainAfterExit = true;
           WorkingDirectory = "${parameters.home}/dotfiles/compose";
-          ExecStart = "${pkgs.docker}/bin/docker compose --env-file ${config.age.secrets.docker-compose-env.path} up --detach --remove-orphans";
-          ExecStop = "${pkgs.docker}/bin/docker compose --env-file ${config.age.secrets.docker-compose-env.path} down";
-          ExecReload = "${pkgs.docker}/bin/docker compose --env-file ${config.age.secrets.docker-compose-env.path} up --detach --remove-orphans";
-        };
-      };
-      chown-volumes = {
-        description = "Set owner and permissions for /mnt/external";
-        serviceConfig = {
-          Type = "oneshot";
-          ExecStart = ''
+          StandardOutput = "journal";
+          StandardError = "journal";
+          ExecStartPre = ''
             ${pkgs.coreutils}/bin/chown -R 1000:1000 /mnt/external
             ${pkgs.coreutils}/bin/chown -R 1000:1000 /var/lib/docker/volumes
             ${pkgs.coreutils}/bin/chmod -R 2775 /mnt/external
             ${pkgs.coreutils}/bin/chmod -R 2775 /var/lib/docker/volumes
+          '';
+          ExecStart = ''
+            ${pkgs.docker}/bin/docker compose --env-file ${config.age.secrets.docker-compose-env.path} down
+            ${pkgs.docker}/bin/docker compose --env-file ${config.age.secrets.docker-compose-env.path} up --detach --remove-orphans
+          '';
+          ExecStop = ''
+            ${pkgs.docker}/bin/docker compose --env-file ${config.age.secrets.docker-compose-env.path} down
           '';
         };
       };

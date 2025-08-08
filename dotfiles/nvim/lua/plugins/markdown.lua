533e732a0eba66156e4a417d359e53fe0da9af31
Pedro Mendes <pedrohrmendes@proton.me>
2025-08-08 00:13:44 -0300

    Update neovim plugins.

--- dotfiles/nvim/lua/plugins/markdown.lua
+++ dotfiles/nvim/lua/plugins/markdown.lua
@@ -1,16 +1,7 @@
 MiniDeps.later(function()
 	MiniDeps.add({ source = "tadmccorkle/markdown.nvim" })
+	MiniDeps.add({ source = "brianhuster/live-preview.nvim" })
 	MiniDeps.add({ source = "davidmh/mdx.nvim", depends = { "nvim-treesitter/nvim-treesitter" } })
-
-	local build = MiniDeps.later(function() vim.fn["mkdp#util#install"]() end)
-
-	MiniDeps.add({
-		source = "iamcco/markdown-preview.nvim",
-		hooks = {
-			post_install = build,
-			post_checkout = build,
-		},
-	})
 
 	require("markdown").setup({
 		on_attach = function(bufnr) require("keymaps.markdown")(bufnr) end,

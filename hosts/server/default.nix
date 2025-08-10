0f8086b5e4b2296db02b535fd626695c7c157726
Pedro Mendes <pedrohrmendes@proton.me>
2025-08-10 12:21:20 -0300

    Add incus to server.

--- hosts/server/default.nix
+++ hosts/server/default.nix
@@ -10,11 +10,17 @@
     ../shared
     ./age.nix
     ./systemd.nix
+    ./virtualization.nix
   ];
 
   hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableAllFirmware;
-  networking.hostName = "server";
   programs.nh.flake = "/home/${parameters.user}/dotfiles";
+
+  networking = {
+    hostName = "server";
+    nftables = true;
+    firewall.trustedInterfaces = [ "incusbr0" ];
+  };
 
   environment.systemPackages = with pkgs; [
     gh
@@ -48,6 +54,51 @@
       device = "/dev/disk/by-label/external";
       fsType = "ext4";
       options = [ "defaults" ];
+    };
+  };
+
+  users.users.${parameters.user}.extraGroups = [ "incus-admin" ];
+
+  virtualisation.incus = {
+    enable = true;
+    preseed = {
+      networks = [
+        {
+          name = "incusbr0";
+          type = "bridge";
+          config = {
+            "ipv4.address" = "10.0.100.1/24";
+            "ipv4.nat" = "true";
+          };
+        }
+      ];
+      profiles = [
+        {
+          name = "default";
+          devices = {
+            eth0 = {
+              name = "eth0";
+              network = "incusbr0";
+              type = "nic";
+            };
+            root = {
+              path = "/";
+              pool = "default";
+              size = "40GiB";
+              type = "disk";
+            };
+          };
+        }
+      ];
+      storage_pools = [
+        {
+          config = {
+            source = "/var/lib/incus/storage-pools/default";
+          };
+          driver = "dir";
+          name = "default";
+        }
+      ];
     };
   };
 

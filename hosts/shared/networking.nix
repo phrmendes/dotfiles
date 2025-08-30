ebf75062143f5caa1bb4e985315fe16920e10e97
Pedro Mendes <pedrohrmendes@proton.me>
2025-08-30 15:07:15 -0300

    Add more nameservers.

--- hosts/shared/networking.nix
+++ hosts/shared/networking.nix
@@ -2,7 +2,10 @@
 {
   networking = {
     useDHCP = lib.mkDefault true;
-    networkmanager.enable = true;
+    networkmanager = {
+      enable = true;
+      insertNameservers = [ "1.1.1.1" "8.8.8.8" "9.9.9.9" ];
+    };
     extraHosts = ''
       127.0.0.1 kubernetes.default.svc.cluster.local
     '';

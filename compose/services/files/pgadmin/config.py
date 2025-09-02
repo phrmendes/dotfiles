63756a6aa8c793a67ab3c87ad08e7027e7947e11
Pedro Mendes <pedrohrmendes@proton.me>
2025-09-02 17:27:45 -0300

    Using adminer.

--- compose/services/files/pgadmin/config.py
+++
@@ -1,9 +0,0 @@
-ENABLE_PSQL = True
-X_FRAME_OPTIONS = "SAMEORIGIN"
-SHARED_STORAGE = [
-    {
-        "name": "dumps",
-        "path": "/var/lib/pgadmin/storage/dumps",
-        "restricted_access": False,
-    }
-]

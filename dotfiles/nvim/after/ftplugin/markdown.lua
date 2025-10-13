e909906d91768ded573a85e3587f9d3adc97786d
Pedro Mendes <pedrohrmendes@proton.me>
2025-10-13 14:14:38 -0300

    Add markdown surround options.

--- dotfiles/nvim/after/ftplugin/markdown.lua
+++ dotfiles/nvim/after/ftplugin/markdown.lua
@@ -1,1 +1,13 @@
 vim.opt_local.shiftwidth = 2
+
+vim.b.minisurround_config = {
+	custom_surroundings = {
+		L = {
+			input = { "%[().-()%]%(.-%)" },
+			output = function()
+				local link = require("mini.surround").user_input("Link: ")
+				return { left = "[", right = "](" .. link .. ")" }
+			end,
+		},
+	},
+}

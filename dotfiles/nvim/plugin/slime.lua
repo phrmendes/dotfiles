3868e57db1bd011b3152c9573ac8920063b6bdf7
Pedro Mendes <pedrohrmendes@proton.me>
2025-12-10 16:34:17 -0300

    Update slime options.

--- dotfiles/nvim/plugin/slime.lua
+++ dotfiles/nvim/plugin/slime.lua
@@ -4,4 +4,13 @@ MiniDeps.later(function()
 	vim.g.slime_target = "neovim"
 	vim.g.slime_bracketed_paste = 1
 	vim.g.slime_no_mappings = true
+	vim.g.slime_menu_config = 1
+	vim.g.slime_neovim_ignore_unlisted = 1
+	vim.g.slime_neovim_menu_delimiter = " | "
+	vim.g.slime_neovim_menu_order = {
+		{ name = "buffer=" },
+		{ pid = "pid=" },
+		{ jobid = "job id=" },
+		{ term_title = "process=" },
+	}
 end)

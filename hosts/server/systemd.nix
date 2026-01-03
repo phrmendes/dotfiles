dc83333d6370654c49971968a3e0a8760d7768d4
Pedro Mendes <pedrohrmendes@proton.me>
2026-01-03 12:20:44 -0300

    Add automatically pull to server.

--- hosts/server/systemd.nix
+++ hosts/server/systemd.nix
@@ -30,46 +30,68 @@
         };
       };
     };
+    timers = {
+      git-pull = {
+        description = "Timer for git pull service";
+        wantedBy = [ "timers.target" ];
+        timerConfig = {
+          OnCalendar = "*-*-* 06:00,18:00";
+          Persistent = true;
+          RandomizedDelaySec = "5m";
+        };
+      };
+    };
     services = {
       nixos-rebuild-switch = {
         description = "NixOS rebuild switch service";
         serviceConfig = {
           Type = "oneshot";
           RemainAfterExit = true;
-          User = "root";
+          User = parameters.user;
+          Group = "users";
           StandardOutput = "journal";
           StandardError = "journal";
-          ExecStart = "${pkgs.nixos-rebuild}/bin/nixos-rebuild switch --flake ${parameters.home}/dotfiles#${config.networking.hostName}";
-          ExecStartPost = "${pkgs.systemd}/bin/systemctl reload docker-compose.service";
+          ExecStart = "${pkgs.sudo}/bin/sudo ${pkgs.nixos-rebuild}/bin/nixos-rebuild switch --flake ${parameters.home}/dotfiles#${config.networking.hostName}";
+          ExecStartPost = "-${pkgs.sudo}/bin/sudo ${pkgs.systemd}/bin/systemctl reload docker-compose.service";
         };
       };
       docker-compose =
         let
-          docker-compose-env-path = config.age.secrets."docker-compose.env".path;
-          compose-cmd = "${pkgs.docker-compose}/bin/docker-compose --env-file=${docker-compose-env-path}";
+          env = config.age.secrets."docker-compose.env".path;
+          compose = "${pkgs.docker-compose}/bin/docker-compose --env-file=${env}";
         in
         {
           description = "Docker Compose services";
-          after = [
-            "docker.service"
-            "network-online.target"
-          ];
+          after = [ "docker.service" ];
           wants = [ "network-online.target" ];
           wantedBy = [ "multi-user.target" ];
           serviceConfig = {
             Type = "oneshot";
             RemainAfterExit = true;
-            User = "root";
+            User = parameters.user;
+            Group = "users";
             WorkingDirectory = "${parameters.home}/dotfiles/compose";
-            ExecStart = "${compose-cmd} up --detach --remove-orphans --force-recreate --pull always";
-            ExecStop = "${compose-cmd} down";
-            ExecReload = "${compose-cmd} down; ${compose-cmd} up --detach --remove-orphans --force-recreate --pull always";
+            ExecStart = "${pkgs.sudo}/bin/sudo ${compose} up --detach --remove-orphans --force-recreate --pull always";
+            ExecStop = "${pkgs.sudo}/bin/sudo ${compose} down";
+            ExecReload = "${pkgs.sudo}/bin/sudo ${compose} down; ${pkgs.sudo}/bin/sudo ${compose} up --detach --remove-orphans --force-recreate --pull always";
             TimeoutStartSec = 0;
             TimeoutStopSec = 300;
             StandardOutput = "journal";
             StandardError = "journal";
           };
         };
+      git-pull = {
+        description = "Git pull dotfiles repository";
+        serviceConfig = {
+          Type = "oneshot";
+          User = parameters.user;
+          Group = "users";
+          WorkingDirectory = "${parameters.home}/dotfiles";
+          ExecStart = "${pkgs.git}/bin/git pull --ff-only";
+          StandardOutput = "journal";
+          StandardError = "journal";
+        };
+      };
     };
   };
 }

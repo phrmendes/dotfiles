728dc142cc118562b10fc2ab185465e3248cb370
Pedro Mendes <pedrohrmendes@proton.me>
2026-02-22 12:35:06 -0300

    Update neovim plugins.

--- hosts/desktop.nix
+++ hosts/desktop.nix
@@ -11,7 +11,7 @@
     extraModprobeConfig = ''options v4l2loopback exclusive_caps=1 card_label="Virtual Camera"'';
     extraModulePackages = with config.boot.kernelPackages; [
       v4l2loopback.out
-      config.boot.kernelPackages.nvidia_x11
+      nvidia_x11
     ];
     kernelModules = [
       "kvm-amd"
@@ -40,7 +40,7 @@
       nvidiaSettings = true;
       modesetting.enable = true;
       powerManagement.enable = false;
-      package = config.boot.kernelPackages.nvidiaPackages.latest;
+      package = config.boot.kernelPackages.nvidiaPackages.stable;
     };
   };
 

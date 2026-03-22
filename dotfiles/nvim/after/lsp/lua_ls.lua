df4e89bcdfc5b9c89fc5e22d5fa8498482a85209
Pedro Mendes <pedrohrmendes@proton.me>
2026-03-22 00:31:54 -0300

    Config lua-ls.

--- dotfiles/nvim/after/lsp/lua_ls.lua
+++ dotfiles/nvim/after/lsp/lua_ls.lua
@@ -1,11 +1,20 @@
 return {
   settings = {
     Lua = {
+      runtime = {
+        version = "LuaJIT",
+      },
       completion = { callSnippet = "Replace" },
       telemetry = { enable = false },
       diagnostics = {
-        globals = { "vim", "Snacks" },
+        globals = { "vim", "Snacks", "ngx", "ndk" },
         disable = { "missing-fields" },
+      },
+      workspace = {
+        library = {
+          "${3rd}/OpenResty/library",
+        },
+        checkThirdParty = false,
       },
     },
   },

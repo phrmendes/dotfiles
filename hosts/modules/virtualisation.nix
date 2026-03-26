ec1b5678f3bc37b9751ba0b33c634a761ea4b426
Pedro Mendes <pedrohrmendes@proton.me>
2026-03-26 20:19:18 -0300

    Fix docker issue.

--- hosts/modules/virtualisation.nix
+++ hosts/modules/virtualisation.nix
@@ -6,6 +6,7 @@
       enable = true;
       storageDriver = "btrfs";
       autoPrune.enable = true;
+      daemon.settings.dns = [ "8.8.8.8" "1.1.1.1" ];
       extraPackages = with pkgs; [
         docker-buildx
         docker-compose

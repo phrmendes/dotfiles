0a96a84951af4b1253106cc11890cb1aa7c1a4ff
Pedro Mendes <pedrohrmendes@proton.me>
2026-04-22 20:33:25 -0300

    lower ip_unprivileged_port_start to 53 for adguardhome DNS

--- modules/virtualisation.nix
+++ modules/virtualisation.nix
@@ -2,7 +2,7 @@ _: {
   modules.nixos.core.virtualisation =
     { pkgs, ... }:
     {
-      boot.kernel.sysctl."net.ipv4.ip_unprivileged_port_start" = 80;
+      boot.kernel.sysctl."net.ipv4.ip_unprivileged_port_start" = 53;
 
       virtualisation = {
         containers.enable = true;

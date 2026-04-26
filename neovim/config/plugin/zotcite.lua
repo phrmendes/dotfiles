a7a9f51dfcd0bd7cdb1958f9661dd91db136000d
Pedro Mendes <pedrohrmendes@proton.me>
2026-04-26 15:43:25 -0300

    Changing zotcite plugin.

--- neovim/config/plugin/zotcite.lua
+++ neovim/config/plugin/zotcite.lua
@@ -1,6 +1,12 @@
-safely("filetype:markdown", function()
+safely("later", function()
   require("zotcite").setup({ pdf_extractor = "pdfnotes2.py" })
   require("zotcite.seek").refs = require("helpers").zotcite_refs
 
-  vim.keymap.set("n", "<leader>z", "", { buffer = true, desc = "+zotcite" })
+  vim.keymap.set("n", "<leader>z", "", { desc = "+zotcite" })
+  vim.keymap.set("i", "<c-x><c-b>", "<Plug>ZCite", { desc = "Insert citation" })
+  vim.keymap.set("n", "<leader>zI", "<Plug>ZCitationCompleteInfo", { desc = "Show complete citation info" })
+  vim.keymap.set("n", "<leader>za", "<Plug>ZExtractAbstract", { desc = "Extract abstract" })
+  vim.keymap.set("n", "<leader>zi", "<Plug>ZCitationInfo", { desc = "Show citation info" })
+  vim.keymap.set("n", "<leader>zo", "<Plug>ZOpenAttachment", { desc = "Open attachment" })
+  vim.keymap.set("n", "<leader>zv", "<Plug>ZViewDocument", { desc = "View document" })
 end)

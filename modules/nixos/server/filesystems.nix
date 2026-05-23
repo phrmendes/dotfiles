c3424a9c66cab64485c6033ea8ca785dc83f3702
Pedro Mendes <pedrohrmendes@proton.me>
2026-05-23 18:08:43 -0300

    Declare /mnt/external/zotero with media group ACL in tmpfiles

--- modules/nixos/server/filesystems.nix
+++ modules/nixos/server/filesystems.nix
@@ -17,6 +17,8 @@ in
     systemd.tmpfiles.rules = [
       "d /run/sync 0700 ${settings.user} users -"
       "d /mnt/external 2775 ${settings.user} media -"
+      "d /mnt/external/zotero 2775 ${settings.user} media -"
+      "a+ /mnt/external/zotero - - - - g:media:rwx,d:g:media:rwx"
       "d /srv 0755 root root -"
       "d /srv/containers 0755 root root -"
     ];

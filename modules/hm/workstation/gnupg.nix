016c6e2c00ae19d0e951aa8858dc3113581e8d06
Pedro Mendes <pedrohrmendes@proton.me>
2026-06-03 03:54:11 -0300

    Add pinentry-gnome3 to home packages.

--- modules/hm/workstation/gnupg.nix
+++ modules/hm/workstation/gnupg.nix
@@ -1,18 +1,23 @@
 {
-  modules.homeManager.workstation.gnupg = _: {
-    programs.gpg = {
-      enable = true;
-      settings = {
-        use-agent = true;
-        no-symkey-cache = true;
+  modules.homeManager.workstation.gnupg =
+    { pkgs, ... }:
+    {
+      home.packages = [ pkgs.pinentry-gnome3 ];
+
+      programs.gpg = {
+        enable = true;
+        settings = {
+          use-agent = true;
+          no-symkey-cache = true;
+        };
+      };
+
+      services.gpg-agent = {
+        enable = true;
+        enableSshSupport = false;
+        pinentryPackage = pkgs.pinentry-gnome3;
+        defaultCacheTtl = 1800;
+        maxCacheTtl = 7200;
       };
     };
-
-    services.gpg-agent = {
-      enable = true;
-      enableSshSupport = false;
-      defaultCacheTtl = 1800;
-      maxCacheTtl = 7200;
-    };
-  };
 }

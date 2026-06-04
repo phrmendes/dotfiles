91c15cc9f2e6fc197f61a4b1bf7db12c6c4ffcb5
Pedro Mendes <pedrohrmendes@proton.me>
2026-06-03 23:50:47 -0300

    Update flakes.

--- flake.nix
+++ flake.nix
@@ -8,7 +8,7 @@
     import-tree.url = "github:vic/import-tree";
     impermanence.url = "github:nix-community/impermanence";
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
-    nixpkgs-stable.url = "github:nixos/nixpkgs?ref=nixos-25.11";
+    nixpkgs-stable.url = "github:nixos/nixpkgs?ref=nixos-26.05";
 
     agenix = {
       url = "github:ryantm/agenix";

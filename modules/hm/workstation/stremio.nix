e0b657e03fa0e7cbf360e72f22ce418d6e26ba76
Pedro Mendes <pedrohrmendes@proton.me>
2026-06-25 02:35:12 -0300

    stremio: fix shortcut

--- modules/hm/workstation/stremio.nix
+++ modules/hm/workstation/stremio.nix
@@ -2,14 +2,17 @@ _: {
   modules.homeManager.workstation.stremio =
     { pkgs, ... }:
     {
-      home.packages = [ pkgs.stremio-linux-shell ];
+      home.packages = with pkgs; [ stremio-linux-shell ];
 
-      xdg.desktopEntries.stremio = {
+      xdg.desktopEntries."com.stremio.Stremio" = {
         name = "Stremio";
+        comment = "Freedom To Stream";
         genericName = "Media Center";
-        exec = "stremio --ozone-platform=x11 %U";
-        icon = "stremio";
+        exec = "stremio --ozone-platform=wayland %U";
+        icon = "com.stremio.Stremio";
         terminal = false;
+        startupNotify = true;
+        mimeType = [ "x-scheme-handler/stremio" ];
         categories = [
           "AudioVideo"
           "Player"

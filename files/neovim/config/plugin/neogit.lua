8d75fc9f290b398dcd41c9b48b715c49e96c453a
Pedro Mendes <pedrohrmendes@proton.me>
2026-07-01 00:20:32 -0300

    neovim: neogit uses auto layout

--- files/neovim/config/plugin/neogit.lua
+++ files/neovim/config/plugin/neogit.lua
@@ -2,6 +2,6 @@ safely("later", function()
   require("neogit").setup({})
   require("codediff").setup()
 
-  vim.keymap.set("n", "<leader>gg", "<cmd>Neogit kind=floating<cr>", { desc = "Neogit" })
+  vim.keymap.set("n", "<leader>gg", "<cmd>Neogit kind=auto<cr>", { desc = "Neogit" })
   vim.keymap.set("n", "<leader>gD", "<cmd>CodeDiff<cr>", { desc = "Diff (repo)" })
 end)

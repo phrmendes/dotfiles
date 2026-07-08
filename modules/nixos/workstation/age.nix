8387dd10103ec33bc96034df6f1e057b88df3a5d
Pedro Mendes <pedrohrmendes@proton.me>
2026-07-08 12:47:48 -0300

    server: remove pi from home-manager, move pi.json agenix secret to workstation-only

--- modules/nixos/workstation/age.nix
+++ modules/nixos/workstation/age.nix
@@ -1,13 +1,15 @@
 { config, ... }:
-let
-  inherit (config) dotfilesLib settings;
-in
 {
-  modules.nixos.workstation.age = {
-    age.secrets."pi.json" = dotfilesLib.mkSecretReadable {
-      user = settings.user;
-      file = ../../../secrets/pi.age.json;
-      path = "${settings.home}/.pi/agent/auth.json";
+  modules.nixos.workstation.age =
+    _:
+    let
+      inherit (config.dotfilesLib) mkSecretReadable;
+    in
+    {
+      age.secrets."pi.json" = mkSecretReadable {
+        user = config.settings.user;
+        file = ../../../secrets/pi.age.json;
+        path = "${config.settings.home}/.pi/agent/auth.json";
+      };
     };
-  };
 }

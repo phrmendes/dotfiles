44a0db05fe819219bc5d1ae8b5e293e4ab5b887e
Pedro Mendes <pedrohrmendes@proton.me>
2026-07-08 02:13:23 -0300

    tmux: update keybindings

--- modules/hm/dev/tmux.nix
+++ modules/hm/dev/tmux.nix
@@ -6,29 +6,49 @@ _: {
         enable = true;
         baseIndex = 1;
         escapeTime = 0;
-        keyMode = "vi";
         mouse = true;
         prefix = "C-space";
         shell = lib.getExe pkgs.zsh;
         extraConfig = ''
-          set -g default-terminal "tmux-256color"
-          set -g extended-keys on
-          set -g extended-keys-format csi-u
-          set -g allow-passthrough on
-          set -g focus-events on
-          set -g set-clipboard off
-          set -g status-position top
-          set -g status-left " #S "
-          set -g status-right ""
+          set -g  default-terminal     tmux-256color
+          set -g  extended-keys        on
+          set -g  extended-keys-format csi-u
+          set -gq allow-passthrough    on
+          set -g detach-on-destroy     off
+          set -g focus-events          on
+          set -g renumber-windows      on
+          set -g set-clipboard         off
+          set -g status                on
+          set -g status-position       top
+          set -g status-left           " #S "
+          set -g status-right          ""
 
-          bind '-' split-window -v -c "#{pane_current_path}"
-          bind '\' split-window -h -c "#{pane_current_path}"
-          bind h select-pane -L
-          bind j select-pane -D
-          bind k select-pane -U
-          bind l select-pane -R
-          bind q kill-pane
-          bind d detach-client
+          set-window-option -g window-status-separator      ""
+          set-window-option -g window-status-format         " #I:#W "
+          set-window-option -g window-status-current-format "#[bold] #I:#W #[nobold]"
+
+          bind '-'   split-window -v -c "#{pane_current_path}"
+          bind ':'   command-prompt
+          bind '\'   split-window -h -c "#{pane_current_path}"
+          bind Enter rotate-window
+          bind R     command-prompt -p "Name:" "new-session -s '%%'"
+          bind d     detach-client
+          bind h     select-pane -L
+          bind j     select-pane -D
+          bind k     kill-session
+          bind k     select-pane -U
+          bind l     select-pane -R
+          bind n     new-window -c "#{pane_current_path}"
+          bind q     kill-pane
+          bind r     command-prompt -I "#W" "rename-window '%%'"
+          bind w     choose-window
+          bind z     resize-pane -Z
+          bind -r H  resize-pane -L 5
+          bind -r J  resize-pane -D 5
+          bind -r K  resize-pane -U 5
+          bind -r L  resize-pane -R 5
+          bind -r [  previous-window
+          bind -r ]  next-window
         '';
       };
     };

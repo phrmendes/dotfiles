c9a8baebfc4704f26e72d597c701efe0f678bc0c
Pedro Mendes <pedrohrmendes@proton.me>
2026-08-08 10:43:54 -0300

    neovim: change indentscope options

--- files/neovim/config/plugin/mini/indentscope.lua
+++ files/neovim/config/plugin/mini/indentscope.lua
@@ -3,7 +3,10 @@ safely(
   function()
     require("mini.indentscope").setup({
       symbol = "▏",
-      draw = { animation = require("mini.indentscope").gen_animation.none() },
+      draw = {
+        animation = require("mini.indentscope").gen_animation.none(),
+        delay = 0,
+      },
       options = { try_as_border = true },
     })
   end
